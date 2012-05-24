9d6f72f376d113032623f5acea36aee3fd261df5
Michel Valin <mfvalin@gmail.com>
2012-05-24 15:32:51 -0400

    continued development

--- rpncomm/RPN_COMM_grid_redist.f
+++ rpncomm/RPN_COMM_grid_redist.f
@@ -54,48 +54,60 @@
       allocate(rdispls(max(pe_nx,pe_ny)))
       allocate(sendcounts(max(pe_nx,pe_ny)))
       allocate(sdispls(max(pe_nx,pe_ny)))
-
+!
+!     zin data shape  (lni,lnj,nk) on PE( 0:pe_nx-1  ,0:pe_ny-1 )
+!
       if(nk <= pe_nx) then    ! alltoallv along x, then reorder, then gather along y
         sendcounts = 1        ! should be 0 but zero length can hang allgatherv
         sendcounts(1:nk) = lni*lnj
-        if(pe_mex < nk) then  ! will be collecting and reordering gni by lnj
-          recvcounts = lni*lnj
-          allocate(recvbuf(lni*lnj*pe_nx))
-          allocate(reorder(gni,lnj))
-        else                  ! dummy collect and no reorder
-          recvcounts = 1
+        if(pe_mex < nk) then    ! real data will only be sent to first nk PEs along X
+          recvcounts = lni*lnj  ! data pieces lni by lnj
+          allocate(recvbuf(lni*lnj*pe_nx))  ! receive buffer for pe_nx data pieces
+          allocate(reorder(gni,lnj))   ! reorder buffer, input to gatherv
+        else
+          recvcounts = 1               ! dummy collect and no need to reorder
           allocate(recvbuf(pe_nx))
           allocate(reorder(1,1))
         endif
-        do i = 1 , pe_mex
-          sdispls(i) = 1 + (min(i,nk)-1)*lni*lnj  ! offset of level I, max nk
-          if(pe_mex < nk) then  ! this pe collects information
-            rdispls(i) = 1 + (i-1)*lni*lnj
+        do i = 1 , pe_nx
+          sdispls(i) = 1 + (min(i,nk)-1)*lni*lnj  ! offset of level I, no greater than nk
+          if(pe_mex < nk) then
+            rdispls(i) = 1 + (i-1)*lni*lnj  ! this pe will collect pe_nx pieces of data
           else
-            rdispls(i) = I      ! this pe only gets one word
+            rdispls(i) = I      ! this pe only gets one word (because zero hangs)
           endif
         enddo
         call MPI_alltoallv(zin,    sendcounts,sdispls,MPI_INTEGER,
      %                     recvbuf,recvcounts,rdispls,MPI_INTEGER,
      %                     pe_myrow,ierr)
+!
+!       recvbuf data shape (lni,lnj,pe_nx) on PE( 0:nk , 0:pe_ny-1 )
+!
         if(pe_mex < nk) then   ! reorder and collect along Y into zout (first nk PEs only)
-          do k = 1 , pe_nx       ! time to reorder
-            offset = (k-1) * lni*lnj  ! offset in recv buffer for PE k, row 1
-            i0 = 1
-            do j = 1 , lnj
-              ilast = min( gni , i0+lni-1 )
-              reorder(i0:ilast,j) = recvbuf(offset+i0:offset+ilast)  ! PE k, row j
+          i0 = 1
+          do k = 1 , pe_nx     ! time to get full x axis together
+            offset = (k-1) * lni*lnj  ! offset in recv buffer for i piece no k, row 1
+            ilast = min( gni , i0+lni-1 )
+            do j = 1 , lnj      ! copy one slice
+              reorder(i0:ilast,j) = recvbuf(offset:offset+ilast-i0)  ! i piece no k, row j
               offset = offset + lnj                                  ! next row
             enddo
+            i0 = i0 + lni   ! next piece of i values
           enddo
+!
+!       reorder data shape (gni,lnj) on PE( 0:nk , 0:pe_ny-1 )
+!
           recvcounts        = gni * lnj
-          recvcounts(pe_ny) = gni * (gnj - lnj*(1-pe_ny))  ! last PEs lnj may be shorter
+          recvcounts(pe_ny) = gni * (gnj - lnj*(pe_ny-1))
           do j = 1 , pe_ny
             rdispls(j) = (j-1) * gni*lnj + 1
           enddo
           call MPI_gatherv(reorder,gni*lnj,            MPI_INTEGER,
      %                     zout,   recvcounts,rdispls, MPI_INTEGER,
      %                     mod(pe_mex,pe_ny),pe_mycol,ierr)      ! root PEs on diagonal
+!
+!       zout data shape (gni,lnj) on PE(l,m) (l=0:nk-1 , m=  l mod pe_ny)
+!
         endif
         goto 1111        ! clean exit
       endif
@@ -115,23 +127,40 @@
         call MPI_alltoallv(zin,    sendcounts,sdispls,MPI_INTEGER,
      %                     recvbuf,recvcounts,rdispls,MPI_INTEGER,
      %                     pe_mycol,ierr)
+!
+!       recvbuf data shape (lni,lnj,pe_ny) on PE( 0:nk-1 ,0:pe_ny-1 )
+!
         if(pe_mey < nk) then                ! gather and reorder
+          recvcounts = lni*gnj              ! lni by gnj slices
+          recvcounts(pe_nx) = gnj * (gni - lni*(pe_nx-1))  ! last PEs lni may be shorter
+          do i = 1 , pe_nx
+            rdispls(i) = (i-1) *lni*gnj + 1
+          enddo
           call MPI_gatherv(recvbuf, lni*gnj,            MPI_INTEGER,
      %                     reorder, recvcounts,rdispls, MPI_INTEGER,
      %                     mod(pe_mey,pe_nx),pe_myrow,ierr)      ! root PEs on diagonal
-         do k = 1 , pe_nx      ! time to reorder
-           i0 = 1
+!
+!       reorder data shape (lni,gnj,pe_nx) on PE(m,l) (l=0:nk-1 , m=  l mod pe_nx)
+!
+         i0 = 1
+         do k = 1 , pe_nx      ! time to reorder, pe_nx slices along i
+           ilast = min( gni , i0+lni-1 )
            do j = 1 , gnj
              offset = (j-1) * lni
-             ilast = min( gni , i0+lni-1 )
-             zout(i0:ilast,j) = reorder(offset+i0:offset+ilast,k)
+             zout(i0:ilast,j) = reorder(offset:offset+ilast-i0,k)
            enddo
+           i0 = i0 + lni
          enddo
         endif
+!
+!       zout data shape (gni,lnj) on PE(l,m) (l=0:nk-1 , m=  l mod pe_ny)
+!
 
         goto 1111        ! clean exit
       endif
-
+!
+!     if we get here OUCH !! (split nk ib slices of max(pe_nx,pe_ny) ?
+!
 1111  deallocate(sendcounts,recvcounts,sdispls,rdispls)
       deallocate(recvbuf,reorder)
       return
