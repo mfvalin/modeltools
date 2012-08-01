f51936601b0b70a5a2e117d087203bcdbdbfee2d
Michel Valin <mfvalin@gmail.com>
2012-08-01 08:45:18 -0400

    udunits version 2 fortran interface

---
+++ tools/test_f_udunits_2.f90
@@ -0,0 +1,10 @@
+	program test_f_udunits_2
+	use ISO_C_BINDING
+	implicit none
+	include 'f_udunits_2.inc'
+	integer, target :: scrap
+	type(ut_system_ptr) :: sys
+
+	sys%ptr = c_loc(scrap)
+	stop
+	end
