f9a1f5a3919aa9b48ac66b9d64d2c03a2084cc3b
Michel Valin <armnmfv@arxt37.cmc.ec.gc.ca>
2012-09-13 17:33:02 -0400

    new grid redist strategy

--- rpncomm/RPN_COMM_grid_redist.f
+++ rpncomm/RPN_COMM_grid_redist.f
@@ -27,13 +27,134 @@
 *              B U G G E D              *
 *                                       *
 *****************************************
-      subroutine RPN_COMM_grid_redist_2(zin,lni,lnj,nk,zout,gni,gnj,outpe_x,noutpe_x,outpe_y,noutpe_y)
+      integer function RPN_COMM_grid_redist(ltok,
+     %           zin,mini,maxi,i0,in,minj,maxj,j0,jn,nk,
+     %           zout,ix0,ixn,jy0,jyn,nz,
+     %           gni,gnj,outpe_x,noutpe_x,outpe_y,noutpe_y)
+      use rpn_comm
+      implicit none
+      integer, intent(IN) :: mini,maxi,i0,in,minj,maxj,j0,jn,nk,ltok
+      integer, intent(IN) :: ix0,ixn,jy0,jyn,nz
+      integer, intent(IN) :: noutpe_x,noutpe_y,gni,gnj
+      integer, dimension(ltok,mini:maxi,minj:maxj,nk), 
+     %         intent(IN) :: zin  ! zin(:,io:in,j0:jn,:) is useful
+      integer, dimension(ix0:ixn,jy0:jyn,nz), intent(OUT) :: zout
+      integer, dimension(noutpe_x) :: outpe_x    ! list of columns where there are PEs doing IO
+      integer, dimension(noutpe_y) :: outpe_y    ! list of rows where there are PEs doing IO
+!
+      integer, dimension(pe_nx) :: istart_g, iend_g, countx, offsetx
+      integer, dimension(pe_nx) :: gdispl_x, gcounts_x, gsize_x
+      integer, dimension(pe_ny) :: jstart_g, jend_g, county, offsety
+      integer, dimension(pe_ny) :: gdispl_y, gcounts_y, gsize_y
+      integer, dimension(noutpe_x) :: level_x
+      integer :: istart, iend, jstart, jend, mxi, mxj
+      integer, allocatable, dimension(:)   :: source, dest
+      integer, allocatable, dimension(:,:) :: source2
+      integer :: lminx, lmaxx, lminy, lmaxy
+      integer :: i, j, k, l
+      integer :: temp, narrays, ierr
+      logical :: needed_for_pass2
+!
+      needed_for_pass2 = .false.
+      RPN_COMM_grid_redist = -1
+      if(nk > noutpe_x*noutpe_y) return !  OUCH !! , or maybe only do the first noutpe_x*noutpe_y
+      narrays = nk
+!
+      call RPN_COMM_limit(pe_mex,pe_nx,1,gni,lminx,lmaxx,countx,offsetx)
+      mxi = countx(1)   ! max tile size along x
+      call RPN_COMM_limit(pe_mey,pe_ny,1,gnj,lminy,lmaxy,county,offsety)
+      mxj = county(1)   ! max tile size along y
+!
+!
+      level_x = 0
+      do k = 1 , narrays
+        temp = 1 + mod(k-1,noutpe_x)
+        level_x(temp) = level_x(temp) + 1
+      enddo
+!
+      do j = 1 , pe_ny   ! start and end of tile(any,j) along y in global space
+        jstart_g(j) = max(jy0,offsety(j))
+        jend_g(j)   = min(jyn,offsetx(j) + county(j) -1)
+      enddo
+      jstart = jstart_g(pe_mey)  ! same as above but for this row
+      jend = jend_g(pe_mey)
+      do i = 1 , pe_nx   ! start and end of tile(i,any) along x in global space
+        istart_g(i) = max(ix0,offsetx(i))
+        iend_g(i)   = min(ixn,offsetx(i) + countx(i) -1)
+        gsize_x(i)  = max(0,iend_g(i)-istart_g(i)+1)  ! horizontal size of tiles on this row
+        gsize_x(i)  = gsize_x(i) * max(0,jend-jstart+1) ! gsize_x may be zero for some tiles
+      enddo
+      istart = istart_g(pe_mex)  ! same as above but for this column
+      iend = iend_g(pe_mex)
+!
+      if( (in-i0-1) * (jn-j0-1) .ne. gsize_x(pe_mex) ) then  ! size consistency problem
+        return  ! add error message above
+      endif
+!
+      if(jstart <= jend) then  ! 
+        k = 1
+        do l = 1 , noutpe_x
+!         calculate counts and displacements for gatherv along x
+          gcounts_x = 1 + gsize_x * level_x(l)  ! horizontal size * number "levels" to gather
+          gdispl_x(1) = 1
+          do i = 2 , pe_nx
+            gdispl_x(i) = gdispl_x(i-1) + gcounts_x(i-1)
+          enddo
+!
+          allocate(source(gcounts_x(pe_mex)))     ! source buffer
+          if(gcounts_x(pe_mex) > 1) then
+!           fill source from zin
+          endif
+          if(pe_mex == outpe_x(l)) then
+            needed_for_pass2 = .true.
+            allocate( dest( gdispl_x(pe_nx)+gcounts_x(pe_nx)) )   ! destination buffer 
+            allocate( source2(ix0:ixn,jstart:jend) )
+          endif
+          call MPI_Gatherv(source,gcounts_x(pe_mex),MPI_INTEGER,
+     %                     dest,gcounts_x,gdispl_x,MPI_INTEGER,
+     %                     outpe_x(l),pe_myrow,ierr)
+          k = k + level_x(l)
+!
+          deallocate(source)
+        enddo
+      else  ! no action on this row, allocate dummy source2 used in y direction gatherv
+        do l = 1 , noutpe_x
+          if(pe_mex == outpe_x(l)) then
+            allocate( source2(1,1))
+            needed_for_pass2 = .true.
+          endif
+        enddo
+      endif   ! (jstart <= jend)
+!
+      if(.not. needed_for_pass2) goto 111
+!
+      do l = 1 , noutpe_y
+        do j = 1 , pe_ny
+          gcounts_y(j) = (ixn-ix0+1) * max(0,jend_g(j)-jstart_g(j)+1)
+        enddo
+        gdispl_y(1) = 1
+        do j = 2 , pe_ny
+          gdispl_y(j) = gdispl_y(j-1) + gcounts_y(j-1)
+        enddo
+!       reorder data for this "level" from dest into source2
+        call MPI_Gatherv(source2,gcounts_y(pe_mey),MPI_INTEGER,
+     %                   zout,gcounts_y,gdispl_y,MPI_INTEGER,
+     %                   outpe_y(l),pe_mycol,ierr)
+      enddo
+      deallocate(dest,source2)
+!
+111   RPN_COMM_grid_redist = 0
+      return
+!
+      end function RPN_COMM_grid_redist
+!
+      subroutine RPN_COMM_grid_redist_2(zin,lni,lnj,nk,zout,gni,gnj,
+     %           outpe_x,noutpe_x,outpe_y,noutpe_y)
 !     zout only needs to be defined on PEs doing I/O (pe_mex in outpe_x AND pe_mey in outpe_y)
 !
 ! NOTE:
 !     lni should probably really be nilmax
 !     lnj should probably really be njlmax
-!     ltok necessaire ?
 !
 !     this MUST be fixed or disaster will strike
 !     we may want to use RPN_COMM_topo2 here to compute things properly
@@ -59,7 +180,7 @@
       integer, allocatable, dimension(:)   :: county, offsety
       integer, dimension(noutpe_x) :: level_x
 !
-      if(nk > noutpe_x*noutpe_y) call abort !  ouch, or maybe only do the  first noutpe_x*noutpe_y
+      if(nk > noutpe_x*noutpe_y) call abort !  OUCH !! , or maybe only do the first noutpe_x*noutpe_y
 !
       allocate(recvcounts(max(pe_nx,pe_ny)))
       allocate(rdispls(max(pe_nx,pe_ny)))
@@ -81,7 +202,8 @@
       enddo
       k = 1
       do l = 1 , noutpe_x
-        if(pe_mex == outpe_x(l)) allocate(recvbuf(mxi*lnj*level_x(l)*pe_nx))
+        if(pe_mex == outpe_x(l)) 
+     %    allocate(recvbuf(mxi*lnj*level_x(l)*pe_nx))
         recvcounts(1) = countx(1)*lnj*level_x(l)
         rdispls(1) = 1
         do i = 2 , pe_nx
@@ -111,6 +233,8 @@
       deallocate(reorder)
 !
       return
+!
+!     ============================== OLD CODE kept for history ==============================
 !
 !     zin data shape  (lni,lnj,nk) on PE( 0:pe_nx-1  ,0:pe_ny-1 )
 !
