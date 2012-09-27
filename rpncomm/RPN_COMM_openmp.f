97b6c47cfaec53bc9d3ab155d4af5562773f3f7e
Michel Valin <armnmfv@euclide.cmc.ec.gc.ca>
2012-09-27 16:05:54 -0400

    used result keyword to avoid PGI compiler error

--- rpncomm/RPN_COMM_openmp.f
+++ rpncomm/RPN_COMM_openmp.f
@@ -7,14 +7,15 @@
 !
 !	no underscore version  (C style)
 !
-	integer(C_INT) function f_omp_get_max_threads()
+	function f_omp_get_max_threads() result(r)
      %                 bind(C,name='f_omp_get_max_threads')
 	use iso_c_binding
 	implicit none
+        integer(C_INT) :: r
 	external :: omp_get_max_threads
 	integer ::  omp_get_max_threads
 
-	f_omp_get_max_threads=omp_get_max_threads()
+	r=omp_get_max_threads()
 	end function f_omp_get_max_threads
 !
 	subroutine f_omp_set_num_threads(nthreads)
@@ -24,18 +25,19 @@
 	integer(C_INT), intent(IN) :: nthreads
 	call omp_set_num_threads(nthreads)
 	return
-	end
+	end subroutine f_omp_set_num_threads
 !
 !	one underscore version
 !
-	integer(C_INT) function f_omp_get_max_threads_()
+	function f_omp_get_max_threads_() result(r)
      %                 bind(C,name='f_omp_get_max_threads_')
 	use iso_c_binding
 	implicit none
+        integer(C_INT) :: r
 	external :: omp_get_max_threads
 	integer ::  omp_get_max_threads
 
-	f_omp_get_max_threads_=omp_get_max_threads()
+	r=omp_get_max_threads()
 	end function f_omp_get_max_threads_
 !
 	subroutine f_omp_set_num_threads_(nthreads)
@@ -49,14 +51,15 @@
 !
 !	two underscores version
 !
-	integer(C_INT) function f_omp_get_max_threads__()
+	function f_omp_get_max_threads__() result(r)
      %                 bind(C,name='f_omp_get_max_threads__')
 	use iso_c_binding
 	implicit none
+        integer(C_INT) :: r
 	external :: omp_get_max_threads
 	integer ::  omp_get_max_threads
 
-	f_omp_get_max_threads__=omp_get_max_threads()
+	r=omp_get_max_threads()
 	end function f_omp_get_max_threads__
 !
 	subroutine f_omp_set_num_threads__(nthreads)
