d67d22f1ceca77b0f6597335b86885ad41d449e9
Michel Valin <mfvalin@gmail.com>
2013-04-23 19:11:40 -0400

    new utility

---
+++ tools/u.run_with_timeout.c
@@ -0,0 +1,54 @@
+/* execve.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv, char **envp) {
+
+  char *newargv[4];
+  char buffer[32768];
+  int bp=0;
+  int i;
+  pid_t pid, pid2;
+  int pidst;
+  int maxwall;
+
+  if (argc < 3) {
+     fprintf(stderr, "Usage: %s <max wall time> <command> [arguments] \n", argv[0]);
+     exit(EXIT_FAILURE);
+  }
+
+  maxwall=atoi(argv[1]);
+  pid=getpid();
+
+  if( fork() ) {
+
+     /* the parent process will execve to bash the requested command */
+
+    newargv[0]="/bin/bash";
+    newargv[1]="-c";
+    newargv[2]=buffer;
+    newargv[3]=NULL;
+
+    for (i=2 ; i<argc ; i++ ){bp+=sprintf(buffer+bp, "%s ",argv[i]);} //fprintf(stderr,"%s\n",buffer);
+
+    execve("/bin/bash", newargv, envp);
+    perror("execve");   /* execve() only returns on error */
+    exit(EXIT_FAILURE);
+
+  }else{
+
+    /* the child watches the parent */
+    while( (maxwall-->0) && ((pidst=kill(pid,0))== 0)) sleep(1)  ;  /* while there is time and first child is alive */
+
+    if(maxwall<=0) { pidst=kill(pid,9) ; }   /* time was up, kill first child */
+    if (pidst==0)
+      fprintf(stderr,"process %d killed, time left=%d\n",pid,++maxwall);
+    else
+      fprintf(stderr,"process %d finished, time left=%d\n",pid,++maxwall);
+    exit (0);
+  }
+
+}
+
