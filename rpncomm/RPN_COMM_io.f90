759ef25a08e3322fef64886d827494f7c91ad0c1
Michel <armnmfv@nemesis.(none)>
2013-11-05 10:51:03 -0500

    refined replication test

--- rpncomm/RPN_COMM_io.f90
+++ rpncomm/RPN_COMM_io.f90
@@ -77,20 +77,20 @@ module RPN_COMM_io  ! file replicator across nodes , async file copy, test and h
   rpn_comm_open=c_rpn_comm_open(c_loc(temp),mode)
   end function rpn_comm_open
 
-  integer function RPN_COMM_file_copy_test()
+  integer function RPN_COMM_file_copy_test()  ! test asynchronous copying in background and file replication across nodes
   implicit none
   include 'mpif.h'
   integer :: fd1, fd2, status, id, ierr, rank
   integer, external :: RPN_COMM_file_copy_start, RPN_COMM_file_bcst
 
   call mpi_init(ierr)
-  call mpi_comm_rank(MPI_COMM_WORLD,rank,ierr)                         ! my rank in communicator
+  call mpi_comm_rank(MPI_COMM_WORLD,rank,ierr)                         ! my rank in world
   rpn_comm_io_debug=.true.
 
   RPN_COMM_file_copy_test = -1
   status = rpn_comm_copy(-1,-1,1)  ! set verbose debug mode
 
-  if(rank /= 0) goto 222  ! first part of test for PE 0 only
+  if(rank /= 0) goto 222  ! first part of test for PE 0 only, asynchronous copying in background 
 
   print *,"START OF COPY TEST"
   fd1 = rpn_comm_open("existing_input_file",0) ! open for read
@@ -101,7 +101,6 @@ module RPN_COMM_io  ! file replicator across nodes , async file copy, test and h
   fd2 = rpn_comm_open("new_output_file",0) ! open for read
   if(fd2 >0 ) then
     print *,"WARNING: new_output_file exists"
-!    return
   endif
   fd2 = rpn_comm_open("new_output_file",1) ! open for write
   if(fd2 <0 ) then
@@ -125,17 +124,17 @@ module RPN_COMM_io  ! file replicator across nodes , async file copy, test and h
   print *,"INFO: cmp new_output_file new_output_file_2"
   call system("cmp new_output_file new_output_file_2 && echo ' INFO: files compare OK'")
 !
-  status = rpn_comm_unlink("new_output_file")
+  status = rpn_comm_unlink("new_output_file")  ! get rid of created files
   if(status /= 0) then
-    print *,"ERROR: failed to delete new_output_file"
+    print *,"WARNING: failed to delete new_output_file"
   endif
   status = rpn_comm_unlink("new_output_file_2")
   if(status /= 0) then
-    print *,"ERROR: failed to delete new_output_file_2"
+    print *,"WARNING: failed to delete new_output_file_2"
   endif
   print *,"END OF COPY TEST"
 
-222 call mpi_barrier(MPI_COMM_WORLD,ierr)  ! all PEs wait for PE 0
+222 call mpi_barrier(MPI_COMM_WORLD,ierr)  ! all PEs wait for PE 0 to start replication test
 
   if(rank == 0) print *,"START OF REPLICATION TEST target_dir/existing_input_file"
   status = RPN_COMM_file_bcst("target_dir/existing_input_file",MPI_COMM_WORLD)
@@ -208,13 +207,13 @@ integer function RPN_COMM_file_bcst(name,com)
   errors = 0
   fd = 0   ! must initialize because some PEs may not call open
   if(rank == 0) then
-    fd = rpn_comm_open(name,0)   ! PE 0, open for read 
-    if(rpn_comm_io_debug) print *,"rank=",rank," read fd=",fd," file=",trim(name)," hostid=",my_color
+    fd = rpn_comm_open(name,0)   ! PE 0, open for read (file MUST exist)
+    if(rpn_comm_io_debug) print 111,"rank=",rank," read  fd=",fd," file=",trim(name)," hostid=",my_color
   else if(rank_on_host ==0) then ! open for write, one process per host, but not if same host as PE 0
     fd = rpn_comm_open(name,1) 
-    if(rpn_comm_io_debug) print *,"rank=",rank," writ fd=",fd," file=",trim(name)," hostid=",my_color
+    if(rpn_comm_io_debug) print 111,"rank=",rank," wriet fd=",fd," file=",trim(name)," hostid=",my_color
   else                           ! nothing to do on this PE
-    if(rpn_comm_io_debug) print *,"rank=",rank," noop fd=",fd," file=",trim(name)," hostid=",my_color
+    if(rpn_comm_io_debug) print 111,"rank=",rank," noop  fd=",fd," file=",trim(name)," hostid=",my_color
   endif
   if(fd < 0) errors = errors + 1
   call mpi_allreduce(errors,sum_errors,1,MPI_INTEGER,MPI_SUM,com,ierr)  !  get global eror count
@@ -227,7 +226,7 @@ integer function RPN_COMM_file_bcst(name,com)
     nwr = buf(0)                                                   ! valid everywhere after broadcast
     nww = buf(0)                                                   ! needed for PEs that do not write to no get errors
     if(rank /= 0 .and. rank_on_host == 0 .and. nwr > 0) then
-      if(rpn_comm_io_debug) print *,"DEBUG: rank no",rank," writing"
+      if(rpn_comm_io_debug) print 111,"DEBUG: rank no",rank," writing"
       nww = rpn_comm_write(fd,c_loc(buf(1)),nwr)  ! one PE per node writes
     endif
     if(nww /= nwr) errors = errors + 1                             ! not everything written, OOPS
@@ -239,8 +238,9 @@ integer function RPN_COMM_file_bcst(name,com)
   RPN_COMM_file_bcst = status
   return
 
+111 format(A,I4,A,I4,A,A,A,Z9)
 999 continue   ! error exception
-  if(rank == 0) print *,"ERROR: RPN_COMM_file_bcst errors =", sum_errors," rank =",rank
+  if(rank == 0) print 111,"ERROR: RPN_COMM_file_bcst errors =", sum_errors," rank =",rank
   RPN_COMM_file_bcst = sum_errors
   return
 
