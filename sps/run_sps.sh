df8e6fd96069b71e26fbd0e96925a912099264f4
Michel Valin <mfvalin@gmail.com>
2015-03-10 17:51:31 -0400

    push

--- sps/run_sps.sh
+++ sps/run_sps.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 [[ "$1" == -v* ]] && set -x
 #
+if [[ -d storage_model ]] ; then
+  storage_model=$(readlink -e storage_model)
+fi
+grep -q storage_model exper.cfg && echo "storage_model=${storage_model}" >>./exper.cfg
+export storage_model
+#
 [[ -r exper.cfg ]] || { echo "ERROR: cannot find $(pwd -P)/exper.cfg" ; exit 1 ; }
 source ./exper.cfg
 #
@@ -10,8 +16,7 @@ source ./exper.cfg
 #
 [[ -z ${exper_fold_date} ]] && exper_fold_date="$(date -d${exper_end_date}+1year  +%Y%m%d)" && echo "exper_fold_date=${exper_fold_date}" >>./exper.cfg
 #
-[[ -d storage_model ]] && storage_model=$(readlink -e storage_model) && echo "storage_model=${storage_model}" >>./exper.cfg
-export storage_model
+[[ gre ]] &&
 #
 [[ -d "${storage_model}" ]] || { echo "ERROR: $storage_model} does not exist" ; exit 1 ; }
 #
