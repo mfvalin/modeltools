2fb750430b9621821d3c1aa6da77cb2ddbf80365
Michel Valin <mfvalin@gmail.com>
2016-03-02 14:57:17 -0500

    robodoc documentation, polishing of .h, revised fortran test

--- x86_utils/cpu_feature_test.F90
+++ x86_utils/cpu_feature_test.F90
@@ -3,12 +3,27 @@ program test_feature
   implicit none
 #define IN_FORTRAN_CODE
 #include <cpu_type.h>
-  if(processor_has_feature(FLAG_AVX2) .ne. 0) print *,'AVX2'
-  if(processor_has_feature(FLAG_AVX) .ne. 0) print *,'AVX'
-  if(processor_has_feature(FLAG_FMA) .ne. 0) print *,'FMA'
-  if(processor_has_feature(FLAG_BMI) .ne. 0) print *,'BMI'
-  if(processor_has_feature(FLAG_SSE4) .ne. 0) print *,'SSE4'
-  if(processor_has_feature(FLAG_SSE3) .ne. 0) print *,'SSE3'
-  if(processor_has_feature(FLAG_SSE2) .ne. 0) print *,'SSE2'
+  real(C_DOUBLE) :: s1, s2
+  integer(C_INT64_T) :: t1, t2
+  if(cpu_has_feature(FLAG_AVX2) .ne. 0) print *,'AVX2'
+  if(cpu_has_feature(FLAG_AVX) .ne. 0) print *,'AVX'
+  if(cpu_has_feature(FLAG_FMA) .ne. 0) print *,'FMA'
+  if(cpu_has_feature(FLAG_BMI) .ne. 0) print *,'BMI'
+  if(cpu_has_feature(FLAG_SSE4) .ne. 0) print *,'SSE4'
+  if(cpu_has_feature(FLAG_SSE3) .ne. 0) print *,'SSE3'
+  if(cpu_has_feature(FLAG_SSE2) .ne. 0) print *,'SSE2'
+  print *,'CPU apicid =',get_cpu_id()
+  s1 = rdtsc_seconds()
+  s2 = rdtsc_seconds()
+  print *,'rdtsc_seconds overhead=',s2
+  s1 = rdtscp_seconds()
+  s2 = rdtscp_seconds()
+  print *,'rdtscp_seconds overhead=',s2
+  t1 = rdtsc()
+  t2 = rdtsc()
+  print *,'rdtsc overhead =',t2-t1,wall_clock_seconds(t2-t1)
+  t1 = rdtscp()
+  t2 = rdtscp()
+  print *,'rdtscp overhead =',t2-t1,wall_clock_seconds(t2-t1)
   stop
 end
--- x86_utils/cpu_type.h
+++ x86_utils/cpu_type.h
@@ -10,17 +10,55 @@
 
 #if defined(IN_FORTRAN_CODE)
   interface
-    function processor_has_feature(feature) result(status) bind(C,name='Cpu_has_feature')
+    function cpu_has_feature(feature) result(status)
       import :: C_INT
       integer(C_INT), intent(IN), value :: feature
       integer(C_INT) :: status
-    end function processor_has_feature
-    function get_processor_apicid() result(id)  bind(C,name='Get_cpu_apicid')
+    end function cpu_has_feature
+
+    function get_cpu_id() result(id)
       import :: C_INT
       integer(C_INT) :: id
-    end function get_processor_apicid
+    end function get_cpu_id
+
+    function get_cpu_freq() result(freq)
+      import C_INT64_T
+      integer(C_INT64_T) :: freq
+    end function get_cpu_freq
+
+    function rdtsc() result(tsc)
+      import C_INT64_T
+      integer(C_INT64_T) :: tsc
+    end function rdtsc
+
+    function rdtscp() result(tsc)
+      import C_INT64_T
+      integer(C_INT64_T) :: tsc
+    end function rdtscp
+
+    function wall_clock_seconds(ticks) result(seconds)
+      import C_INT64_T, C_DOUBLE
+      integer(C_INT64_T), intent(IN), value :: ticks
+      real(C_DOUBLE) :: seconds
+    end function wall_clock_seconds
+
+    function rdtscp_seconds() result(seconds)
+      import C_DOUBLE
+      real(C_DOUBLE) :: seconds
+    end function rdtscp_seconds
+
+    function rdtsc_seconds() result(seconds)
+      import C_DOUBLE
+      real(C_DOUBLE) :: seconds
+    end function rdtsc_seconds
   end interface
 #else
-  int Cpu_has_feature(int feature);
-  int Get_cpu_apicid();
+  int cpu_has_feature(int feature);
+  int get_cpu_id(void);
+  uint64_t get_cpu_freq(void);
+  uint64_t rdtsc(void);
+  uint64_t rdtscp(void);
+  double wall_clock_seconds(uint64_t ticks);
+  double rdtsc_seconds(void);
+  double rdtscp_seconds(void);
 #endif
