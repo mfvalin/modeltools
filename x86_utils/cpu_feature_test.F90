3f36b8e7c3b53cbaf76ae8a454e95daa7bb1b73c
Michel Valin <mfvalin@gmail.com>
2016-03-04 16:03:40 -0500

    added floating point status word interface

--- x86_utils/cpu_feature_test.F90
+++ x86_utils/cpu_feature_test.F90
@@ -4,7 +4,9 @@ program test_feature
 #define IN_FORTRAN_CODE
 #include <cpu_type.h>
   real(C_DOUBLE) :: s1, s2
+  real :: r
   integer(C_INT64_T) :: t1, t2
+  integer(C_INT) :: status
   if(cpu_has_feature(FLAG_AVX2) .ne. 0) print *,'AVX2'
   if(cpu_has_feature(FLAG_AVX) .ne. 0) print *,'AVX'
   if(cpu_has_feature(FLAG_FMA) .ne. 0) print *,'FMA'
@@ -12,6 +14,25 @@ program test_feature
   if(cpu_has_feature(FLAG_SSE4) .ne. 0) print *,'SSE4'
   if(cpu_has_feature(FLAG_SSE3) .ne. 0) print *,'SSE3'
   if(cpu_has_feature(FLAG_SSE2) .ne. 0) print *,'SSE2'
+
+  status = get_fp_status_ctl()
+  print 100,'FP status = ',status
+  if(iand(FP_STATUS_PE,status) .ne. 0) print *,"Precision ON"
+  if(iand(FP_STATUS_UE,status) .ne. 0) print *,"Underflow ON"
+  if(iand(FP_STATUS_OE,status) .ne. 0) print *,"Overflow ON"
+  if(iand(FP_STATUS_ZE,status) .ne. 0) print *,"Zero divide ON"
+100 format(A,z8.8)
+  r = 1.0E-30; r = r*r; /* underflow */
+  r = 1.0/r;            /* zero divide */
+  r = 1.0E30; r = r*r;  /* overflow */
+  status = get_fp_status_ctl()
+  print *,"forcing underflow, zero divide, overflow"
+  print 100,'FP status = ',status
+  if(iand(FP_STATUS_PE,status) .ne. 0) print *,"Precision ON"
+  if(iand(FP_STATUS_UE,status) .ne. 0) print *,"Underflow ON"
+  if(iand(FP_STATUS_OE,status) .ne. 0) print *,"Overflow ON"
+  if(iand(FP_STATUS_ZE,status) .ne. 0) print *,"Zero divide ON"
+
   print *,'CPU apicid =',get_cpu_id()
   s1 = rdtsc_seconds()
   s2 = rdtsc_seconds()
