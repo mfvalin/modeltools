4e729c8dbe498d240e59bb7b75891dfcacada6d8
Michel Valin <mfvalin@gmail.com>
2016-08-29 19:31:06 -0400

    linux x86 ification, with comments, will return zeroes in not x86_84 + linux

--- x86_utils/rdtsc_plus_id.c
+++ x86_utils/rdtsc_plus_id.c
@@ -1,23 +1,30 @@
 #include <stdlib.h>
 #include<stdio.h>
 
-unsigned long tacc_rdtscp(int *chip, int *core)
+unsigned long long rdtscp(int *socket, int *processor)  // get tsc/socket/processor
 {
-   unsigned long int x;
-   unsigned a, d, c;
-
+#if defined(__x86_64__) &&  defined(__linux__)
+   unsigned int a, d, c;
+   // rdtscp instruction
+   // EDX:EAX contain TimeStampCounter
+   // ECX contains IA32_TSC_AUX[31:0] (MSR_TSC_AUX value set by OS, lower 32 bits contain socket+processor)
    __asm__ volatile("rdtscp" : "=a" (a), "=d" (d), "=c" (c));
-    *chip = (c & 0xFFF000)>>12;
-    *core = c & 0xFFF;
+    *socket = (c & 0xFFF000)>>12;
+    *processor = c & 0xFFF;
 
-   return ((unsigned long)a) | (((unsigned long)d) << 32);;
+   return ((unsigned long long)a) | (((unsigned long long)d) << 32);;
+#else
+   *socket = 0;
+   *processor = 0;
+   return 0;
+#endif
 }
-#if defined SELF_TEST
+#if defined(SELF_TEST)
 int main(int argc, char **argv) {
-  int chip, core;
-  chip = -1; core = 01;
-  unsigned long tsc = tacc_rdtscp(&chip, &core);
-  printf("chip = %d, core = %d \n",chip,core);
+  int socket, processor;
+  socket = -1; processor = 01;
+  unsigned long long tsc = rdtscp(&socket, &processor);
+  printf("socket = %d, processor = %d \n",socket,processor);
   return 0;
 }
 #endif
