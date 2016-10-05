ee3b6b9aa30fd01d0b02f4aef018e7afc9232e30
Michel Valin <mfvalin@gmail.com>
2016-10-05 10:10:38 -0400

    fixes to demo jobs

--- CONTAINERS/taskset.sh
+++ CONTAINERS/taskset.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export MP_CHILD=${MP_CHILD:-${PMI_RANK:-${OMPI_COMM_WORLD_RANK:-${ALPS_APP_PE}}}}
 echo "CHILD = $MP_CHILD, $(taskset -cp $$) on $(hostname)"
+touch $FLAGDIR/FLAG_$MP_CHILD
