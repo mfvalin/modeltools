2ed10206e2514d9630ceff7d6d669e5acefb4f64
Michel Valin <mfvalin@gmail.com>
2016-12-25 19:13:51 -0500

    int transpose seems to work

--- x86_utils/transpose_avx.c
+++ x86_utils/transpose_avx.c
@@ -1,7 +1,7 @@
 #include <immintrin.h>
 
 // NOTE: Fortran order assumed for matrices
-void transpose_int_by_8(void *a, int la1, void *b, int lb1, int ni, int nj){
+void TransposeBy4bytes(void *a, int la1, void *b, int lb1, int ni, int nj){
 #if defined(NEVER_TRUE)
   basic block, 8x8 transpose of 32 bit elements within 256 bit AVX/AVX2 registers
   3 pass shuffle, first 2 operations are within 128 bit lane, last one is across 128 bit lanes
@@ -22,7 +22,7 @@ permHH uses _mm256_permute2f128_si256 (a,b, 0x31), bot128 = top128 from a, top12
 #endif
   int *a00, *a01, *b00, *b01;
   int *a000, *b000;
-  int i, j;
+  int i, j, j0;
   int la2 = la1 + la1;
   int la3 = la2 + la1;
   int la4 = la3 + la1;
@@ -91,14 +91,31 @@ permHH uses _mm256_permute2f128_si256 (a,b, 0x31), bot128 = top128 from a, top12
       a00 = a00 + 8;           // bump by 8 cols
       b00 = b00 + lb4 + lb4;   // bump by 8 rows
     } // for i
-    for (i = ni8 ; i<ni ; i++){  // leftovers along i , j -> j+7
+    for ( ; i<ni ; i++){  // leftovers along i , j -> j+7
+      b00[0] = a00[  0] ;  // b[j  ,i] = a[i,j  ]
+      b00[1] = a00[la1] ;  // b[j+1,i] = a[i,j+1]
+      b00[2] = a00[la2] ;  // b[j+2,i] = a[i,j+2]
+      b00[3] = a00[la3] ;  // b[j+3,i] = a[i,j+3]
+      b00[4] = a00[la4    ] ;  // b[j+4,i] = a[i,j+4]
+      b00[5] = a00[la4+la1] ;  // b[j+5,i] = a[i,j+5]
+      b00[6] = a00[la4+la2] ;  // b[j+6,i] = a[i,j+6]
+      b00[7] = a00[la4+la3] ;  // b[j+7,i] = a[i,j+7]
+      b00 += lb1;
+      a00++;
     }
     a000 = a000 + la4 + la4;   // bump by 8 rows
     b000 = b000 + 8;           // bump by 8 cols
   }  // for j
+  a000 = (int *)a;
+  b000 = (int *)b;
+  for (i = 0 ; i<ni ; i++){    // leftovers along j, i = 0 -> nj-1
+    for(j0=j ; j0<nj ; j0++){
+      b000[j0+i*lb1] = a000[i+j0*la1];   // b[j,i] = a[i,j]
+    }
+  }
 }
 
-void transpose_long_by_8(double *a, int la1, double *b, int lb1, int ni, int nj){
+void TransposeBy8bytes(void *a, int la1, void *b, int lb1, int ni, int nj){
   // after transpose, b[j,i] = a[i,j]
   int la2 = la1 + la1;
   int la3 = la2 + la1;
@@ -106,96 +123,100 @@ void transpose_long_by_8(double *a, int la1, double *b, int lb1, int ni, int nj)
   int lb2 = lb1 + lb1;
   int lb3 = lb2 + lb1;
   int lb4 = lb3 + lb1;
-  int i, j;
-  double *a00, *a01, *a10, *a11, *b00, *b01, *b10, *b11;
-  __m256d y0, y1, y2, y3;
-  __m256d t0, t1, t2, t3;
-  __m256d x0, x1, x2, x3;
+  int i, j, ni8, nj8;
+  long long *a000, *a00, *a01, *a10, *a11, *b000, *b00, *b01, *b10, *b11;
+  __m256i y0, y1, y2, y3;
+  __m256i t0, t1, t2, t3;
+  __m256i x0, x1, x2, x3;
 
-  a00 = a ; a01 = a + 4 ; a10 = a00 + la4 ; a11 = a10 + 4;
-  b00 = b ; b01 = b + 4 ; b10 = b00 + lb4 ; b11 = b10 + 4;
-  for (j=0 ; j<nj ; j+= 8){
-    for ( i=0 ; i<ni ; i+=8){
+  a000 = (long long *)a;
+  b000 = (long long *)b;
+  ni8 = ni & 0x7FFFFFF8 ;   // lower multiple of 8
+  nj8 = nj & 0x7FFFFFF8 ;   // lower multiple of 8
+  for (j=0 ; j<nj8 ; j+= 8){
+    for ( i=0 ; i<ni8 ; i+=8){
+      a00 = &a000[i + j*la1] ; a01 = a00 + 4 ; a10 = a00 + la4 ; a11 = a10 + 4;
+      b00 = &b000[j + i*lb1] ; b01 = b00 + 4 ; b10 = b00 + lb4 ; b11 = b10 + 4;
       // basic 8 x 8 transpose (a00, a01, a10, a11 into b00, b10, b01, b11) ( 4 4x4 sub matrices)
-      y0 = _mm256_loadu_pd(&a00[  0]);   // fetch 4 x 4 from a00
-      y1 = _mm256_loadu_pd(&a00[la1]);
-      y2 = _mm256_loadu_pd(&a00[la2]);
-      y3 = _mm256_loadu_pd(&a00[la3]);
+      y0 = _mm256_loadu_si256((__m256i const *)&a00[  0]);   // fetch 4 x 4 from a00
+      y1 = _mm256_loadu_si256((__m256i const *)&a00[la1]);
+      y2 = _mm256_loadu_si256((__m256i const *)&a00[la2]);
+      y3 = _mm256_loadu_si256((__m256i const *)&a00[la3]);
 
-      t0 = _mm256_unpackhi_pd(y0,y1);    // shuffle pass 1 on a00 -> b00
-      t1 = _mm256_unpackhi_pd(y2,y3);
-      t2 = _mm256_unpacklo_pd(y0,y1);
-      t3 = _mm256_unpacklo_pd(y2,y3);
+      t0 = _mm256_unpackhi_epi64(y0,y1);    // shuffle pass 1 on a00 -> b00
+      t1 = _mm256_unpackhi_epi64(y2,y3);
+      t2 = _mm256_unpacklo_epi64(y0,y1);
+      t3 = _mm256_unpacklo_epi64(y2,y3);
 
-      y0 = _mm256_loadu_pd(&a01[  0]);   // prefetch 4 x 4 from a01
-      y1 = _mm256_loadu_pd(&a01[la1]);
-      y2 = _mm256_loadu_pd(&a01[la2]);
-      y3 = _mm256_loadu_pd(&a01[la3]);
+      y0 = _mm256_loadu_si256((__m256i const *)&a01[  0]);   // prefetch 4 x 4 from a01
+      y1 = _mm256_loadu_si256((__m256i const *)&a01[la1]);
+      y2 = _mm256_loadu_si256((__m256i const *)&a01[la2]);
+      y3 = _mm256_loadu_si256((__m256i const *)&a01[la3]);
 
-      x0 = _mm256_permute2f128_pd(t2,t3,0x20);    // shuffle pass 2 on a00 -> b00
-      x1 = _mm256_permute2f128_pd(t0,t1,0x20);
-      x2 = _mm256_permute2f128_pd(t2,t3,0x31);
-      x3 = _mm256_permute2f128_pd(t0,t1,0x31);
+      x0 = _mm256_permute2f128_si256(t2,t3,0x20);    // shuffle pass 2 on a00 -> b00
+      x1 = _mm256_permute2f128_si256(t0,t1,0x20);
+      x2 = _mm256_permute2f128_si256(t2,t3,0x31);
+      x3 = _mm256_permute2f128_si256(t0,t1,0x31);
 
-      _mm256_storeu_pd(&b00[  0],x0);   // store 4 x 4 into b00 (from a00)
-      _mm256_storeu_pd(&b00[lb1],x1);
-      _mm256_storeu_pd(&b00[lb2],x2);
-      _mm256_storeu_pd(&b00[lb3],x3);
+      _mm256_storeu_si256((__m256i *)&b00[  0],x0);   // store 4 x 4 into b00 (from a00)
+      _mm256_storeu_si256((__m256i *)&b00[lb1],x1);
+      _mm256_storeu_si256((__m256i *)&b00[lb2],x2);
+      _mm256_storeu_si256((__m256i *)&b00[lb3],x3);
 
-      t0 = _mm256_unpackhi_pd(y0,y1);    // shuffle pass 1 on a01 -> b10
-      t1 = _mm256_unpackhi_pd(y2,y3);
-      t2 = _mm256_unpacklo_pd(y0,y1);
-      t3 = _mm256_unpacklo_pd(y2,y3);
+      t0 = _mm256_unpackhi_epi64(y0,y1);    // shuffle pass 1 on a01 -> b10
+      t1 = _mm256_unpackhi_epi64(y2,y3);
+      t2 = _mm256_unpacklo_epi64(y0,y1);
+      t3 = _mm256_unpacklo_epi64(y2,y3);
 
-      y0 = _mm256_loadu_pd(&a10[  0]);   // prefetch 4 x 4 from a10
-      y1 = _mm256_loadu_pd(&a10[la1]);
-      y2 = _mm256_loadu_pd(&a10[la2]);
-      y3 = _mm256_loadu_pd(&a10[la3]);
+      y0 = _mm256_loadu_si256((__m256i const *)&a10[  0]);   // prefetch 4 x 4 from a10
+      y1 = _mm256_loadu_si256((__m256i const *)&a10[la1]);
+      y2 = _mm256_loadu_si256((__m256i const *)&a10[la2]);
+      y3 = _mm256_loadu_si256((__m256i const *)&a10[la3]);
 
-      x0 = _mm256_permute2f128_pd(t2,t3,0x20);    // shuffle pass 2 on a01 -> b10
-      x1 = _mm256_permute2f128_pd(t0,t1,0x20);
-      x2 = _mm256_permute2f128_pd(t2,t3,0x31);
-      x3 = _mm256_permute2f128_pd(t0,t1,0x31);
+      x0 = _mm256_permute2f128_si256(t2,t3,0x20);    // shuffle pass 2 on a01 -> b10
+      x1 = _mm256_permute2f128_si256(t0,t1,0x20);
+      x2 = _mm256_permute2f128_si256(t2,t3,0x31);
+      x3 = _mm256_permute2f128_si256(t0,t1,0x31);
 
-      _mm256_storeu_pd(&b10[  0],x0);   // store 4 x 4 into b10 (from a01)
-      _mm256_storeu_pd(&b10[lb1],x1);
-      _mm256_storeu_pd(&b10[lb2],x2);
-      _mm256_storeu_pd(&b10[lb3],x3);
+      _mm256_storeu_si256((__m256i *)&b10[  0],x0);   // store 4 x 4 into b10 (from a01)
+      _mm256_storeu_si256((__m256i *)&b10[lb1],x1);
+      _mm256_storeu_si256((__m256i *)&b10[lb2],x2);
+      _mm256_storeu_si256((__m256i *)&b10[lb3],x3);
 
-      t0 = _mm256_unpackhi_pd(y0,y1);    // shuffle pass 1 on a10 -> b01
-      t1 = _mm256_unpackhi_pd(y2,y3);
-      t2 = _mm256_unpacklo_pd(y0,y1);
-      t3 = _mm256_unpacklo_pd(y2,y3);
+      t0 = _mm256_unpackhi_epi64(y0,y1);    // shuffle pass 1 on a10 -> b01
+      t1 = _mm256_unpackhi_epi64(y2,y3);
+      t2 = _mm256_unpacklo_epi64(y0,y1);
+      t3 = _mm256_unpacklo_epi64(y2,y3);
 
-      y0 = _mm256_loadu_pd(&a11[  0]);   // prefetch 4 x 4 from a11
-      y1 = _mm256_loadu_pd(&a11[la1]);
-      y2 = _mm256_loadu_pd(&a11[la2]);
-      y3 = _mm256_loadu_pd(&a11[la3]);
+      y0 = _mm256_loadu_si256((__m256i const *)&a11[  0]);   // prefetch 4 x 4 from a11
+      y1 = _mm256_loadu_si256((__m256i const *)&a11[la1]);
+      y2 = _mm256_loadu_si256((__m256i const *)&a11[la2]);
+      y3 = _mm256_loadu_si256((__m256i const *)&a11[la3]);
 
-      x0 = _mm256_permute2f128_pd(t2,t3,0x20);    // shuffle pass 2 on a10 -> b01
-      x1 = _mm256_permute2f128_pd(t0,t1,0x20);
-      x2 = _mm256_permute2f128_pd(t2,t3,0x31);
-      x3 = _mm256_permute2f128_pd(t0,t1,0x31);
+      x0 = _mm256_permute2f128_si256(t2,t3,0x20);    // shuffle pass 2 on a10 -> b01
+      x1 = _mm256_permute2f128_si256(t0,t1,0x20);
+      x2 = _mm256_permute2f128_si256(t2,t3,0x31);
+      x3 = _mm256_permute2f128_si256(t0,t1,0x31);
 
-      _mm256_storeu_pd(&b01[  0],x0);   // store 4 x 4 into b01 (from a10)
-      _mm256_storeu_pd(&b01[lb1],x1);
-      _mm256_storeu_pd(&b01[lb2],x2);
-      _mm256_storeu_pd(&b01[lb3],x3);
+      _mm256_storeu_si256((__m256i *)&b01[  0],x0);   // store 4 x 4 into b01 (from a10)
+      _mm256_storeu_si256((__m256i *)&b01[lb1],x1);
+      _mm256_storeu_si256((__m256i *)&b01[lb2],x2);
+      _mm256_storeu_si256((__m256i *)&b01[lb3],x3);
 
-      t0 = _mm256_unpackhi_pd(y0,y1);    // shuffle pass 1 on a11 -> b11
-      t1 = _mm256_unpackhi_pd(y2,y3);
-      t2 = _mm256_unpacklo_pd(y0,y1);
-      t3 = _mm256_unpacklo_pd(y2,y3);
+      t0 = _mm256_unpackhi_epi64(y0,y1);    // shuffle pass 1 on a11 -> b11
+      t1 = _mm256_unpackhi_epi64(y2,y3);
+      t2 = _mm256_unpacklo_epi64(y0,y1);
+      t3 = _mm256_unpacklo_epi64(y2,y3);
 
-      x0 = _mm256_permute2f128_pd(t2,t3,0x20);    // shuffle pass 2 on a11 -> b11
-      x1 = _mm256_permute2f128_pd(t0,t1,0x20);
-      x2 = _mm256_permute2f128_pd(t2,t3,0x31);
-      x3 = _mm256_permute2f128_pd(t0,t1,0x31);
+      x0 = _mm256_permute2f128_si256(t2,t3,0x20);    // shuffle pass 2 on a11 -> b11
+      x1 = _mm256_permute2f128_si256(t0,t1,0x20);
+      x2 = _mm256_permute2f128_si256(t2,t3,0x31);
+      x3 = _mm256_permute2f128_si256(t0,t1,0x31);
 
-      _mm256_storeu_pd(&b11[  0],x0);   // store 4 x 4 into b11 (from a11)
-      _mm256_storeu_pd(&b11[lb1],x1);
-      _mm256_storeu_pd(&b11[lb2],x2);
-      _mm256_storeu_pd(&b11[lb3],x3);
+      _mm256_storeu_si256((__m256i *)&b11[  0],x0);   // store 4 x 4 into b11 (from a11)
+      _mm256_storeu_si256((__m256i *)&b11[lb1],x1);
+      _mm256_storeu_si256((__m256i *)&b11[lb2],x2);
+      _mm256_storeu_si256((__m256i *)&b11[lb3],x3);
     }
   }
 }
@@ -203,13 +224,13 @@ void transpose_long_by_8(double *a, int la1, double *b, int lb1, int ni, int nj)
 #if defined(SELF_TEST)
 #include <stdio.h>
 #include <stdlib.h>
-#define NI 17
-#define NJ 15
-main(){
+#define NI 19
+#define NJ 21
+main(int argc, char **argv){
   double mtx1_4x4[NI*NJ];
   double mtx2_4x4[NI*NJ];
-  float mat1_4x4[NI*NJ];
-  float mat2_4x4[NI*NJ];
+  int mat1_4x4[NI*NJ];
+  int mat2_4x4[NI*NJ];
   int i, j;
   for (i=0 ; i<NI*NJ ; i++){
     mtx1_4x4[i] = i;
@@ -229,7 +250,7 @@ main(){
   for (i=0 ; i<NI ; i++) printf("%4d",i) ;
   printf(">\n");
 
-  transpose_long_by_8(mtx1_4x4, NI, mtx2_4x4, NJ, 8, 8);  // 4 x 4 transpose
+  TransposeBy8bytes(mtx1_4x4, NI, mtx2_4x4, NJ, 16, 16);
   printf("\nOutput Matrix (double)\n");
   for(j=NI-1 ; j>= 0 ; j--){
     printf("row %2d ",j);
@@ -242,12 +263,12 @@ main(){
   for (i=0 ; i<NJ ; i++) printf("%4d",i) ;
   printf(">\n");
 
-  transpose_int_by_8(mat1_4x4, NI, mat2_4x4, NJ, 8, 8);  // 4 x 4 transpose
+  TransposeBy4bytes(mat1_4x4, NI, mat2_4x4, NJ,atoi(argv[1]), atoi(argv[2]));
   printf("\nOutput Matrix (float)\n");
   for(j=NI-1 ; j>= 0 ; j--){
     printf("row %2d ",j);
     for (i=0 ; i<NJ ; i++){
-      printf("%4.0f",mat2_4x4[i+NJ*j]);
+      printf("%4d",mat2_4x4[i+NJ*j]);
     }
     printf("\n");
   }
