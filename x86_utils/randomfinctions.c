5f8643ce1d7f50210e5e379d3d9e44af66fc2a50
Michel Valin <mfvalin@gmail.com>
2017-01-09 17:11:53 -0500

    introduced stream with state  versions

--- x86_utils/randomfinctions.c
+++ x86_utils/randomfinctions.c
@@ -74,6 +74,12 @@ void GetInitialSeeds(unsigned int auiSeed[], int cSeed, unsigned int uiSeed, uns
  * R250 ans SHR3 generators added, naming is consistent with the MWC8222 code
  *==========================================================================*/
 /*------------------------ start of SHR3 addition --------------------------*/
+typedef struct{
+  unsigned long jsr;
+} shr3_state;
+
+static shr3_state shr3 = { 123456789 } ;
+
 static unsigned long jsr=123456789;
 // #define SHR3 (jz=jsr, jsr^=(jsr<<13), jsr^=(jsr>>17), jsr^=(jsr<<5),jz+jsr)
 
@@ -140,7 +146,43 @@ void VecDRan_SHR3(double *ranbuf, int n){
 
 /*------------------------- end of SHR3 addition ---------------------------*/
 /*------------------------ start of R250 addition --------------------------*/
-static unsigned int r250_buffer[250] = {
+typedef struct{
+  int index;
+  unsigned int buffer[250];
+}r250_state ;
+
+static r250_state r250 = {
+  0 ,
+  {
+    0x17617168 ,0x17a0d192 ,0x7cba449a ,0x86d91b38 ,0x7455bfb5 ,0x3bb194f2 ,0xd4cf89e2 ,0xee85f453 ,0x916c5ad3 ,0x8e5c32bb ,
+    0x0d035201 ,0xc45d76fd ,0x04d799be ,0x409aa63c ,0x4c56a663 ,0xe0404838 ,0x88141c55 ,0xeb963899 ,0x83f34bc2 ,0xcf784dfe ,
+    0x303c4f3d ,0xe2e580de ,0x9213c2a7 ,0x13a77f75 ,0xd0b5ca05 ,0xfc21d156 ,0x1e4c4c31 ,0xe4d70461 ,0xc8d3eb8a ,0x19d5a61d ,
+    0x40d8f714 ,0x051457d9 ,0x60dd589d ,0xadcae8f4 ,0xae50f8e4 ,0xc0b1623d ,0x019840e2 ,0x9fd91e54 ,0x205fd793 ,0x9e17be66 ,
+    0xc55b8dfa ,0x2fc0cfc5 ,0x7f292738 ,0x826e9e1f ,0x50e160e4 ,0xb3a2c76f ,0x5e03e011 ,0xe05601d4 ,0x5ef981a0 ,0x8676f0f1 ,
+    0x6d8690cb ,0x8af13789 ,0xab00a515 ,0x2bcff371 ,0xd4adf100 ,0xdca5d50d ,0xc4ebad34 ,0x0d75d8f3 ,0xf77b9800 ,0x5593a16e ,
+    0x1504475c ,0x453356ee ,0x1302dbd3 ,0xad1d21af ,0xcf668d00 ,0x76d68c1c ,0x91b61c7e ,0x831fbcb8 ,0x96258a86 ,0xbd1f4791 ,
+    0xdf26ff85 ,0x93f5aa8d ,0xf6f6d072 ,0x55622d0f ,0x64905fb1 ,0x2d4b22c3 ,0x1ca372c3 ,0xda494a98 ,0xcfa8f513 ,0xf4737e2e ,
+    0xe77f1a9c ,0x238ff343 ,0xafa9f948 ,0x20823fdf ,0x010c0e27 ,0x0020f353 ,0x31654bfd ,0x9637394d ,0x5ffd52b6 ,0x06db7185 ,
+    0x76fadbb5 ,0xc90c3dd1 ,0x71364d2a ,0x49b411a8 ,0xda7556c9 ,0x86a61957 ,0xba798498 ,0x442d2c73 ,0xc89268b0 ,0x77d1be52 ,
+    0x326e6b29 ,0x727a3092 ,0xba7b0780 ,0xf751def0 ,0xc1c05141 ,0x774f8101 ,0xde7495b4 ,0x2250658f ,0xd896e6e2 ,0x04a1649d ,
+    0xf0dbdc2f ,0xab8a806e ,0xcccaef53 ,0x24a9daf8 ,0x9c472081 ,0xe88534a7 ,0xc6400afc ,0x5db66a6c ,0xf1bcdb68 ,0xe283ac5c ,
+    0x71f6bb93 ,0x29bc3b72 ,0x6cc0d398 ,0x7ad7b430 ,0x07f8067d ,0x9bc21534 ,0xcad1a0be ,0x2eb81bf9 ,0x866e596e ,0xe6150f60 ,
+    0x4f0cdac2 ,0xf14feeb3 ,0x63559552 ,0xee360d3e ,0xe950794d ,0x5674b5b7 ,0x8866636f ,0xb3bb5604 ,0xf894278a ,0xc4a13b2b ,
+    0xc07f38ca ,0x1742de68 ,0xda9d902f ,0x2cb57fbc ,0x050fabca ,0x6b471777 ,0x4bdee5be ,0x60d3d78a ,0x6b31001b ,0x237156a5 ,
+    0x617f2b07 ,0x4cbe6264 ,0x05610cd5 ,0x3d222b24 ,0x097ffff2 ,0x38975335 ,0x8682e0dc ,0x7e993479 ,0xe1ecf67b ,0xc619babd ,
+    0x0f1ac989 ,0x1eab4e4b ,0x8c3cb3fc ,0x5787c983 ,0x74f19f89 ,0x968f257d ,0x95cc62b2 ,0x11e6bd09 ,0xd1a57e05 ,0x67358a7f ,
+    0x95e23779 ,0x30efec41 ,0xe46c4803 ,0x2d2414e1 ,0x352c0fda ,0x1da1a740 ,0x28aea00b ,0xfe1dec28 ,0xae7b6c47 ,0xcfd1de31 ,
+    0xa468360d ,0x544fc9e5 ,0xbcd04aa4 ,0xd2cfc38b ,0xb8a48f82 ,0xa8718902 ,0x5bd8a509 ,0x9c40dd86 ,0x6a3dadd0 ,0xd0a0d65f ,
+    0xc62298c6 ,0x46393aca ,0x0b7436f2 ,0x99ddd69c ,0x839b79a7 ,0xa155be69 ,0x2e4f0458 ,0x474bd538 ,0x73d65578 ,0xa49ab70f ,
+    0xbe2a3c0b ,0x69e550db ,0x9e38abcb ,0x9e483578 ,0xdabc5814 ,0x2e73f8ef ,0x4ed45df8 ,0x05f8d621 ,0x0259c01e ,0xf3927074 ,
+    0xfda21b64 ,0x3476f241 ,0x9aa5d95a ,0xef86ea14 ,0x8f3fce06 ,0x8bff6bfa ,0x706ab0a2 ,0x7322f175 ,0x4e8acb27 ,0x336889cc ,
+    0x373ea2e0 ,0x0cc5f5ce ,0x35a5cc68 ,0x93169549 ,0xea31a7b1 ,0x6a6569bc ,0xa776f509 ,0x5b0f310e ,0x96322244 ,0x64568c56 ,
+    0x08aa6767 ,0x491799f1 ,0x17735c88 ,0x71c32f7e ,0xed0a2ec6 ,0xebd94777 ,0x9b1e1086 ,0xdc740f7a ,0x03c48151 ,0xafcb9f88 ,
+    0xd835a40a ,0x21308fc2 ,0x0f459e5e ,0x0358b165 ,0x6422fa89 ,0xdd9cf11b ,0x03daccf5 ,0xec9e2bd9 ,0xe300013e ,0xa97d54e4 
+  }
+};
+
+static unsigned int r250buffer[250] = {
 0x17617168 ,0x17a0d192 ,0x7cba449a ,0x86d91b38 ,0x7455bfb5 ,0x3bb194f2 ,0xd4cf89e2 ,0xee85f453 ,0x916c5ad3 ,0x8e5c32bb ,
 0x0d035201 ,0xc45d76fd ,0x04d799be ,0x409aa63c ,0x4c56a663 ,0xe0404838 ,0x88141c55 ,0xeb963899 ,0x83f34bc2 ,0xcf784dfe ,
 0x303c4f3d ,0xe2e580de ,0x9213c2a7 ,0x13a77f75 ,0xd0b5ca05 ,0xfc21d156 ,0x1e4c4c31 ,0xe4d70461 ,0xc8d3eb8a ,0x19d5a61d ,
@@ -168,9 +210,26 @@ static unsigned int r250_buffer[250] = {
 0xd835a40a ,0x21308fc2 ,0x0f459e5e ,0x0358b165 ,0x6422fa89 ,0xdd9cf11b ,0x03daccf5 ,0xec9e2bd9 ,0xe300013e ,0xa97d54e4 };
 static int r250_index = 250 ;
 
+static void FillBuffer_R250_stream(r250_state *r250){
+  int i;
+  unsigned int *r250_buffer = r250->buffer ;
+  int r250_index = r250->index;
+
+  while(r250_index > 249) r250_index -= 250;
+  for (i=0 ; i< 147 ; i++) {
+    r250_buffer[ i ] = r250_buffer[ i ] ^ r250_buffer[ i + 103 ];
+  }
+  for (i=147 ; i<250 ; i++) {
+    r250_buffer[ i ] = r250_buffer[ i ] ^ r250_buffer[ i - 147 ];
+  }
+  r250->index = r250_index;
+}
+
 static void FillBuffer_R250(void){
   int i;
-  while(r250_index > 249) r250_index -= 250;
+  unsigned int *r250_buffer = r250.buffer ;
+
+  while(r250.index > 249) r250.index -= 250;
   for (i=0 ; i< 147 ; i++) {
     r250_buffer[ i ] = r250_buffer[ i ] ^ r250_buffer[ i + 103 ];
   }
@@ -184,9 +243,9 @@ void RanSetSeed_R250(int *piSeed, int cSeed)
   int i;
 
   if (cSeed == 250 && piSeed != NULL) {
-    for(i=0 ; i<250; i++) r250_buffer[ i ] = piSeed[ i ] ;
+    for(i=0 ; i<250; i++) r250.buffer[ i ] = piSeed[ i ] ;
   }else{
-    GetInitialSeeds(r250_buffer, 250, piSeed && cSeed ? piSeed[0] : 0, 0);
+    GetInitialSeeds(r250.buffer, 250, piSeed && cSeed ? piSeed[0] : 0, 0);
   }
 }
 
@@ -194,8 +253,8 @@ double DRan_R250(void)		/* returns a random double (0.0 , 1.0) */
 {
   register int	i, j;
   register unsigned int new_rand;
-  if ( r250_index > 249 ) FillBuffer_R250();
-  new_rand = r250_buffer[r250_index++] ;
+  if ( r250.index > 249 ) FillBuffer_R250();
+  new_rand = r250.buffer[r250.index++] ;
   return RANDBL_32new(new_rand);   // convert from 32 bit int to (0.0 , 1.0)
 }
 
@@ -203,8 +262,8 @@ double DRanS_R250(void)		/* returns a random double (-1.0 , 1.0) */
 {
   register int	i, j;
   register unsigned int new_rand;
-  if ( r250_index > 249 ) FillBuffer_R250();
-  new_rand = r250_buffer[r250_index++] ;
+  if ( r250.index > 249 ) FillBuffer_R250();
+  new_rand = r250.buffer[r250.index++] ;
   return RANDBLS_32new(new_rand);   // convert from 32 bit int to (0.0 , 1.0)
 }
 
@@ -212,49 +271,76 @@ unsigned int IRan_R250(void)		/* returns a random unsigned integer */
 {
   register int	i, j;
   register unsigned int new_rand;
-  if ( r250_index > 249 ) FillBuffer_R250();
-  new_rand = r250_buffer[r250_index++];
+  if ( r250.index > 249 ) FillBuffer_R250();
+  new_rand = r250.buffer[r250.index++];
   return new_rand;
 }
 
 void VecIRan_R250(unsigned int *ranbuf, int n){
   int k = 0;
   int i;
+  unsigned int *r250_buffer = r250.buffer ;
+
+  while( r250.index < 250 && n > 0 ){
+    ranbuf[k++] = r250_buffer[r250.index++] ;
+    n-- ;
+  }
+  if ( n == 0 ) return;
+  FillBuffer_R250_stream(&r250);     // we get here if buffer is empty before n is satisfied
+  while(n >= 250){        // chunks of 250 values
+    for(i=0 ; i<250 ; i++) ranbuf[k+i] = r250_buffer[i] ;
+    n -= 250 ;
+    k += 250 ;
+    FillBuffer_R250_stream(&r250) ;
+  }
+  while( n > 0 ){  // n < 250 at this point
+    ranbuf[k++] = r250_buffer[r250.index++] ;
+    n-- ;
+  }
+}
+
+void VecIRan_R250_stream(r250_state *r250, unsigned int *ranbuf, int n){
+  int k = 0;
+  int i;
+  unsigned int *r250_buffer = r250->buffer ;
+  int r250_index = r250->index ;
+
   while( r250_index < 250 && n > 0 ){
     ranbuf[k++] = r250_buffer[r250_index++] ;
     n-- ;
   }
   if ( n == 0 ) return;
-  FillBuffer_R250();     // we get here if buffer is empty before n is satisfied
+  FillBuffer_R250_stream(r250);     // we get here if buffer is empty before n is satisfied
   while(n >= 250){        // chunks of 250 values
     for(i=0 ; i<250 ; i++) ranbuf[k+i] = r250_buffer[i] ;
     n -= 250 ;
     k += 250 ;
-    FillBuffer_R250() ;
+    FillBuffer_R250_stream(r250) ;
   }
   while( n > 0 ){  // n < 250 at this point
     ranbuf[k++] = r250_buffer[r250_index++] ;
     n-- ;
   }
+  r250->index = r250_index;
 }
 
 void VecDRan_R250(double *ranbuf, int n){
   int k = 0;
   int i;
-  while( r250_index < 250 && n > 0 ){
-    ranbuf[k++] = RANDBL_32new(r250_buffer[r250_index++]) ;   // convert from 32 bit int to (0.0 , 1.0)
+  while( r250.index < 250 && n > 0 ){
+    ranbuf[k++] = RANDBL_32new(r250.buffer[r250.index++]) ;   // convert from 32 bit int to (0.0 , 1.0)
     n-- ;
   }
   if ( n == 0 ) return;
   FillBuffer_R250();     // we get here if buffer is empty
   while(n >= 250){        // chunks of 250 values
-    for(i=0 ; i<250 ; i++) ranbuf[k+i] = RANDBL_32new(r250_buffer[i]) ;   // convert from 32 bit int to (0.0 , 1.0)
+    for(i=0 ; i<250 ; i++) ranbuf[k+i] = RANDBL_32new(r250.buffer[i]) ;   // convert from 32 bit int to (0.0 , 1.0)
     n -= 250 ;
     k += 250 ;
     FillBuffer_R250() ;
   }
   while( n > 0 ){  // n < 250 at this point
-    ranbuf[k++] = RANDBL_32new(r250_buffer[r250_index++]) ;   // convert from 32 bit int to (0.0 , 1.0)
+    ranbuf[k++] = RANDBL_32new(r250.buffer[r250.index++]) ;   // convert from 32 bit int to (0.0 , 1.0)
     n-- ;
   }
 }
@@ -264,88 +350,97 @@ void VecDRan_R250(double *ranbuf, int n){
 #define MWC_A  LIT_UINT64(809430660)
 #define MWC_AI 809430660
 #define MWC_C  362436
+
+typedef struct{
+   unsigned int uiState ;
+   unsigned int uiCarry ;
+   unsigned int auiState[MWC_R];
+} mwc_state ;
+
+static mwc_state mwc;
+
 static unsigned int s_uiStateMWC = MWC_R - 1;
 static unsigned int s_uiCarryMWC = MWC_C;
 static unsigned int s_auiStateMWC[MWC_R];
 
 void RanSetSeed_MWC8222(int *piSeed, int cSeed)
 {
-	s_uiStateMWC = MWC_R - 1;
-	s_uiCarryMWC = MWC_C;
+	mwc.uiState = MWC_R - 1;
+	mwc.uiCarry = MWC_C;
 	
 	if (cSeed == MWC_R)
 	{
 		int i;
 		for (i = 0; i < MWC_R; ++i)
 		{
-			s_auiStateMWC[i] = (unsigned int)piSeed[i];
+			mwc.auiState[i] = (unsigned int)piSeed[i];
 		}
 	}
 	else
 	{
-		GetInitialSeeds(s_auiStateMWC, MWC_R, piSeed && cSeed ? piSeed[0] : 0, 0);
+		GetInitialSeeds(mwc.auiState, MWC_R, piSeed && cSeed ? piSeed[0] : 0, 0);
 	}
 }
 unsigned int IRan_MWC8222(void)
 {
 	UINT64 t;
 
-	s_uiStateMWC = (s_uiStateMWC + 1) & (MWC_R - 1);
-	t = MWC_A * s_auiStateMWC[s_uiStateMWC] + s_uiCarryMWC;
-	s_uiCarryMWC = (unsigned int)(t >> 32);
-	s_auiStateMWC[s_uiStateMWC] = (unsigned int)t;
+	mwc.uiState = (mwc.uiState + 1) & (MWC_R - 1);
+	t = MWC_A * mwc.auiState[mwc.uiState] + mwc.uiCarry;
+	mwc.uiCarry = (unsigned int)(t >> 32);
+	mwc.auiState[mwc.uiState] = (unsigned int)t;
     return (unsigned int)t;
 }
 double DRan_MWC8222(void)         /* returns a random double (0.0 , 1.0) */
 {
 	UINT64 t;
 
-	s_uiStateMWC = (s_uiStateMWC + 1) & (MWC_R - 1);
-	t = MWC_A * s_auiStateMWC[s_uiStateMWC] + s_uiCarryMWC;
-	s_uiCarryMWC = (unsigned int)(t >> 32);
-	s_auiStateMWC[s_uiStateMWC] = (unsigned int)t;
+	mwc.uiState = (mwc.uiState + 1) & (MWC_R - 1);
+	t = MWC_A * mwc.auiState[mwc.uiState] + mwc.uiCarry;
+	mwc.uiCarry = (unsigned int)(t >> 32);
+	mwc.auiState[mwc.uiState] = (unsigned int)t;
 	return RANDBL_32new(t);   // convert from 32 bit int to (0.0 , 1.0)
 }
 double DRanS_MWC8222(void)        /* returns a random double (-1.0 , 1.0) */
 {
 	UINT64 t;
 
-	s_uiStateMWC = (s_uiStateMWC + 1) & (MWC_R - 1);
-	t = MWC_A * s_auiStateMWC[s_uiStateMWC] + s_uiCarryMWC;
-	s_uiCarryMWC = (unsigned int)(t >> 32);
-	s_auiStateMWC[s_uiStateMWC] = (unsigned int)t;
+	mwc.uiState = (mwc.uiState + 1) & (MWC_R - 1);
+	t = MWC_A * mwc.auiState[mwc.uiState] + mwc.uiCarry;
+	mwc.uiCarry = (unsigned int)(t >> 32);
+	mwc.auiState[mwc.uiState] = (unsigned int)t;
 	return RANDBLS_32new(t);   // convert from 32 bit int to (-1.0 , 1.0)
 }
 void VecIRan_MWC8222(unsigned int *auiRan, int cRan)
 {
 	UINT64 t;
-	unsigned int carry = s_uiCarryMWC, state = s_uiStateMWC;
+	unsigned int carry = mwc.uiCarry, state = mwc.uiState;
 	
 	for (; cRan > 0; --cRan, ++auiRan)
 	{
 		state = (state + 1) & (MWC_R - 1);
-		t = MWC_A * s_auiStateMWC[state] + carry;
-		*auiRan = s_auiStateMWC[state] = (unsigned int)t;
+		t = MWC_A * mwc.auiState[state] + carry;
+		*auiRan = mwc.auiState[state] = (unsigned int)t;
 		carry = (unsigned int)(t >> 32);
 	}
-	s_uiCarryMWC = carry;
-	s_uiStateMWC = state;
+	mwc.uiCarry = carry;
+	mwc.uiState = state;
 }
 void VecDRan_MWC8222(double *adRan, int cRan)
 {
 	UINT64 t;
-	unsigned int carry = s_uiCarryMWC, state = s_uiStateMWC;
+	unsigned int carry = mwc.uiCarry, state = mwc.uiState;
 	
 	for (; cRan > 0; --cRan, ++adRan)
 	{
 		state = (state + 1) & (MWC_R - 1);
-		t = MWC_A * s_auiStateMWC[state] + carry;
-		s_auiStateMWC[state] = (unsigned int)t;
+		t = MWC_A * mwc.auiState[state] + carry;
+		mwc.auiState[state] = (unsigned int)t;
 		*adRan = RANDBL_32new(t);   // convert from 32 bit int to (0.0 , 1.0)
 		carry = (unsigned int)(t >> 32);
 	}
-	s_uiCarryMWC = carry;
-	s_uiStateMWC = state;
+	mwc.uiCarry = carry;
+	mwc.uiState = state;
 }
 /*----------------------- END George Marsaglia MWC -------------------------*/
 
@@ -447,18 +542,18 @@ static double s_adZigX[ZIGNOR_C + 1], s_adZigR[ZIGNOR_C];
 static unsigned int kn[128];
 static double wn[128],fn[128];
 
-static unsigned int zigcalls = 0;
-static unsigned int zigquick = 0;
-static unsigned int zigtails = 0;
-static unsigned int zigwedge = 0;
-static unsigned int zigloops = 0;
-static unsigned int zigused  = 0;
-static unsigned int zigcalls2 = 0;
-static unsigned int zigquick2 = 0;
-static unsigned int zigtails2 = 0;
-static unsigned int zigwedge2 = 0;
-static unsigned int zigloops2 = 0;
-static unsigned int zigused2 = 0;
+INSTRUMENT(static unsigned int zigcalls = 0;)
+INSTRUMENT(static unsigned int zigquick = 0;)
+INSTRUMENT(static unsigned int zigtails = 0;)
+INSTRUMENT(static unsigned int zigwedge = 0;)
+INSTRUMENT(static unsigned int zigloops = 0;)
+INSTRUMENT(static unsigned int zigused  = 0;)
+INSTRUMENT(static unsigned int zigcalls2 = 0;)
+INSTRUMENT(static unsigned int zigquick2 = 0;)
+INSTRUMENT(static unsigned int zigtails2 = 0;)
+INSTRUMENT(static unsigned int zigwedge2 = 0;)
+INSTRUMENT(static unsigned int zigloops2 = 0;)
+INSTRUMENT(static unsigned int zigused2 = 0;)
 
 #define ZIGNOR_STORE 256 * 4
 static unsigned int s_auiZigTmp[ZIGNOR_STORE + ZIGNOR_STORE / 4];
@@ -492,7 +587,7 @@ static double DRanNormalTail(double dMin, int iNegative)
 //     y = DRanU();
     y = RANDBL_32new(i_adZigRan[--s_cZigStored]);
     y = log(y);
-    zigused += 2;
+    INSTRUMENT(zigused += 2;)
   } while (-2 * y < x * x);
   return iNegative ? x - dMin : dMin - x;
 }
@@ -797,16 +892,16 @@ main(int argc, char **argv){
   printf("maxpos, maxneg transformed with RANDBLS_32new : %22.18f %22.18f , %16.16Lx, %16.16Lx\n",dmax,dmin,*idmax,*idmin);
 
    RanSetSeed_MWC8222(&lr, 1);
-   RanNormalSetSeedZig(r250_buffer, 250);   // initialize to values already there :-)
-   RanNormalSetSeedZigFast(r250_buffer, 250);   // initialize to values already there :-)
+   RanNormalSetSeedZig(r250.buffer, 250);   // initialize to values already there :-)
+   RanNormalSetSeedZigFast(r250.buffer, 250);   // initialize to values already there :-)
 
-//   printf("static unsigned int r250_buffer[250] = {\n");
+//   printf("static unsigned int r250.buffer[250] = {\n");
   for (i=0 ; i<25 ; i++){
     for( j=0 ; j<10 ; j++){
       lr = (unsigned int) mrand48();
 //       printf("0x%8.8x ",lr);
 //       if(i==24 && j==9) printf("};") ; else  printf(",") ;
-      if(r250_buffer[j+10*i] != lr) exit(1);
+      if(r250.buffer[j+10*i] != lr) exit(1);
     }
 //     printf("\n");
   }
@@ -925,10 +1020,12 @@ main(int argc, char **argv){
   printf("\n");
 
   for( i=0 ; i < 10 ; i++) VecIRan_R250(ranbuf, 1000000) ;
+//   for( i=0 ; i < 10 ; i++) VecIRan_R250_stream(&r250,ranbuf, 1000000) ;
   MPI_Barrier(MPI_COMM_WORLD);
   t0 = MPI_Wtime();
   for( i=0 ; i < 1000 ; i++){
     VecIRan_R250(ranbuf, 1000000) ;
+//     VecIRan_R250_stream(&r250,ranbuf, 1000000) ;
   }
   t1 = MPI_Wtime();
 //   pos = 0 ; neg = 0 ; for( i=0 ; i < 1000000 ; i++) if((int)ranbuf[i] > 0) pos++ ; else neg++ ;
@@ -936,6 +1033,7 @@ main(int argc, char **argv){
   postot = 0 ; negtot = 0;
   for (j=0 ; j<100 ; j++) {
     VecIRan_R250(ranbuf, 1000000) ;
+//     VecIRan_R250_stream(&r250,ranbuf, 1000000) ;
     mask = 1 ;
     while (mask) {
       pos = 0 ; neg = 0 ; 
@@ -1055,27 +1153,28 @@ main(int argc, char **argv){
   t1 = MPI_Wtime();
   printf("time for 1E+6 x 1E+3 random SHR3 double values = %6.3f \n",t1-t0);
 
-  t1 = zigquick ; t0 = zigcalls+1 ; 
-  printf("quick calls in ziggurat = %6.3f%\n",t1 / t0 * 100.0);
-  t1 = zigtails ;
-  printf("tail  calls in ziggurat = %6.3f%\n",t1 / t0 * 100.0);
-  t1 = zigwedge ;
-  printf("wedge calls in ziggurat = %6.3f%\n",t1 / t0 * 100.0);
-  t1 = zigloops ;
-  printf("extra loops in ziggurat = %6.3f%\n",t1 / t0 * 100.0);
-  t1 = zigused ;
-  printf("random values used in ziggurat = %6.3f%\n",t1 / t0 * 100.0);
+  t1 = 0 ; t0 = 1 ; 
+  INSTRUMENT(t1 = zigquick ; t0 = zigcalls+1 ; )
+  INSTRUMENT(printf("quick calls in ziggurat = %6.3f%\n",t1 / t0 * 100.0);)
+  INSTRUMENT(t1 = zigtails ;)
+ INSTRUMENT( printf("tail  calls in ziggurat = %6.3f%\n",t1 / t0 * 100.0);)
+  INSTRUMENT(t1 = zigwedge ;)
+  INSTRUMENT(printf("wedge calls in ziggurat = %6.3f%\n",t1 / t0 * 100.0);)
+  INSTRUMENT(t1 = zigloops ;)
+  INSTRUMENT(printf("extra loops in ziggurat = %6.3f%\n",t1 / t0 * 100.0);)
+  INSTRUMENT(t1 = zigused ;)
+  INSTRUMENT(printf("random values used in ziggurat = %6.3f%\n",t1 / t0 * 100.0);)
 
-  t1 = zigquick2 ; t0 = zigcalls2+1 ; 
-  printf("quick calls in ziggurat = %6.3f%\n",t1 / t0 * 100.0);
-  t1 = zigtails2 ;
-  printf("tail  calls in ziggurat = %6.3f%\n",t1 / t0 * 100.0);
-  t1 = zigwedge2 ;
-  printf("wedge calls in ziggurat = %6.3f%\n",t1 / t0 * 100.0);
-  t1 = zigloops2 ;
-  printf("extra loops in ziggurat = %6.3f%\n",t1 / t0 * 100.0);
-  t1 = zigused2 ;
-  printf("random values used in ziggurat = %6.3f%\n",t1 / t0 * 100.0);
+  INSTRUMENT(t1 = zigquick2 ; t0 = zigcalls2+1 ; )
+  INSTRUMENT(printf("quick calls in ziggurat = %6.3f%\n",t1 / t0 * 100.0);)
+  INSTRUMENT(t1 = zigtails2 ;)
+  INSTRUMENT(printf("tail  calls in ziggurat = %6.3f%\n",t1 / t0 * 100.0);)
+  INSTRUMENT(t1 = zigwedge2 ;)
+  INSTRUMENT(printf("wedge calls in ziggurat = %6.3f%\n",t1 / t0 * 100.0);)
+  INSTRUMENT(t1 = zigloops2 ;)
+  INSTRUMENT(printf("extra loops in ziggurat = %6.3f%\n",t1 / t0 * 100.0);)
+  INSTRUMENT(t1 = zigused2 ;)
+  INSTRUMENT(printf("random values used in ziggurat = %6.3f%\n",t1 / t0 * 100.0);)
 
   MPI_Finalize();
 }
