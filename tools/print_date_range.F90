320bd845441c4d3866e65e09669b3442ad2ae890
Michel Valin <mfvalin@gmail.com>
2017-05-16 15:31:25 -0400

    added directory and link creation

--- tools/print_date_range.F90
+++ tools/print_date_range.F90
@@ -1,4 +1,5 @@
 program print_date_range
+  use ISO_C_BINDING
   implicit none
   integer, external :: newdate
   integer :: yyyymmdd, hhmmss, stamp, p1, p2, stamp1, stamp2, p3, p4, stamp3
@@ -6,13 +7,42 @@ program print_date_range
   real *8 :: delta, diff
   integer :: status, argcount
   character(len=128) :: date1, date2, interval, name, options
+  character(len=32) :: arg1, arg2
+  character(len=4096) :: oldpath, newpath, dirpath
+  character(len=4096) :: nest_rept, nest_exp, anal
+  character(len=1), dimension(1) :: template
+  integer(C_INT) :: mode
+
+  interface
+    function f_mkdir(path,mode) result(status) bind(C,name='mkdir')
+      import :: C_CHAR, C_INT
+      character(C_CHAR), intent(IN) :: path
+      integer(C_INT), intent(IN), value :: mode
+      integer(C_INT) :: result
+    end function f_mkdir
+    function f_link(oldpath,newpath) result(status) bind(C,name='link')
+      import :: C_CHAR, C_INT
+      character(C_CHAR), intent(IN) :: oldpath, newpath
+      integer(C_INT) :: result
+    end function f_link
+    function f_symlink(oldpath,newpath) result(status) bind(C,name='symlink')
+      import :: C_CHAR, C_INT
+      character(C_CHAR), intent(IN) :: oldpath, newpath
+      integer(C_INT) :: result
+    end function f_symlink
+  end interface
 
   CALL get_command_argument(0, name)
 
   argcount = command_argument_count()
-  if(argcount < 3 .or. argcount > 4) goto 777
+  if(argcount < 6 .or. argcount > 7) goto 777
+  mode = o'0777'
 
   CALL get_command_argument(1, date1)
+! newpath = './soft_link'
+! status = f_symlink( transfer(trim(oldpath)//achar(0),template(1)), transfer(trim(newpath)//achar(0),template(1)) )
+! print *,'status =',status
+! stop
   date1 = trim(date1)//'000000'
   read(date1,11,err=777)printable1(1),printable1(2)  ! start date in YYYYMMDD format
 11 format(I8,1x,I6)
@@ -25,11 +55,22 @@ program print_date_range
   read(interval,*,err=777)delta                      ! interval in seconds
   delta = delta/3600.0                               ! interval in hours
 
-  if(argcount == 4) then                             ! year=option is present
+  CALL get_command_argument(4, anal)
+  CALL get_command_argument(5, nest_rept)
+  CALL get_command_argument(6, nest_exp)
+!   nest_rept = 'CLIMAT_nest_rept'
+!   nest_exp  = 'CLIMAT_nest_exp'
+!   anal = 'GEM_anal'
+
+  if(argcount == 7) then                             ! year=option is present
     CALL get_command_argument(4, options)
     call NewDate_Options(trim(options),'set')        ! set calendar option
     write(0,*),'INFO: using calendqar option '//trim(options)
   endif
+
+  open(unit=11,file='content',form='FORMATTED')
+  write(11,'(A/A)')'1','GEM_input_file_0001'
+  close(unit=11)
 
   status = newdate(stamp1,printable1(1),printable1(2)*100,3) ! stamp for start date
   status = newdate(stamp2,printable2(1),printable2(2)*100,3) ! stamp for end date
@@ -42,7 +83,18 @@ program print_date_range
       call incdatr(stamp3,stamp1,-24.0_8)
       status = newdate(stamp3,p3,p4,-3)
     endif
-    print 102,p1,'.',p2/100,p3                        ! print it
+!     print 102,p1,'.',p2/100,p3                        ! print it
+    write(arg1,'(I8.8,A1,I6.6)')p1,'.',p2/100
+    write(arg2,'(I8.8)')p3
+    print *,trim(arg1),' ',trim(arg2)
+    write(dirpath,'(A)')'VALID_'//trim(arg1)
+    status = f_mkdir( transfer(trim(dirpath)//achar(0),template(1)), mode )
+    oldpath = 'content'
+    newpath = trim(dirpath)//'/content'
+    status = f_link( transfer(trim(oldpath)//achar(0),template(1)), transfer(trim(newpath)//achar(0),template(1)) )
+    oldpath = trim(nest_rept) // '/' // trim(nest_exp) // '_' // arg2(1:6) // '/' // trim(nest_exp) // '_' // arg2(1:8)
+    newpath = 'VALID_' // trim(arg1) // '/GEM_input_file_0001'
+    status = f_symlink( transfer(trim(oldpath)//achar(0),template(1)), transfer(trim(newpath)//achar(0),template(1)) )
     call incdatr(stamp,stamp1,delta)                  ! increment
     stamp1 = stamp
     call difdatr(stamp2,stamp1,diff)                  ! end - next date
