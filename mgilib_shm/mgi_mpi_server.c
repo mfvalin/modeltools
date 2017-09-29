5b471dfaef5e63c5ffb67b03af663a82e24376f6
Michel Valin <mfvalin@gmail.com>
2017-09-29 16:50:23 -0400

    added licence

--- mgilib_shm/mgi_mpi_server.c
+++ mgilib_shm/mgi_mpi_server.c
@@ -1,3 +1,22 @@
+/* useful routines for C and FORTRAN programming
+ * Copyright (C) 2017  Division de Recherche en Prevision Numerique, Environnement Canada
+ * Copyright (C) 2017  Centre ESCER UQAM
+ *
+ * This code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this code; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 #include "mpi.h" 
 #define MAX_DATA 128
 #define FATAL 1
