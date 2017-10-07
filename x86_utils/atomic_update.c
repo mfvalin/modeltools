d2a6a2eb5fd59a5b4ff0d73651f7c2cd1f05b5fa
Michel Valin <mfvalin@gmail.com>
2017-10-06 21:16:29 -0400

    added fetch and add function

--- x86_utils/atomic_update.c
+++ x86_utils/atomic_update.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdint.h>
 
-atomic_add(int *what,int n) {
+  atomic_add(int *what,int n) {
   __asm__ __volatile__(
       "   lock       ;\n"
       "   addl %1,%0 ;\n"
@@ -39,14 +40,43 @@ atomic_xor(int *what,int n) {
       :
       );
 }
+
+int atomic_fetch_and_add(int* what, int n)
+{
+  __asm__ volatile(
+      "   lock       ;\n"
+      "   xaddl %0, %1"
+      : "+r" (n), "+m" (*what) // input+output
+      : // No input-only
+      : "memory"
+    );
+    return n;
+}
 #if defined(SELF_TEST)
+
+uint64_t rdtsc(void) {   // version rapide "out of order"
+#if defined(__x86_64__) || defined( __i386__ )
+  uint32_t lo, hi;
+  __asm__ volatile ("rdtscp"
+      : /* outputs */ "=a" (lo), "=d" (hi)
+      : /* no inputs */
+      : /* clobbers */ "%rcx");
+  return (uint64_t)lo | (((uint64_t)hi) << 32);
+#else
+  return time0++;
+#endif
+}
+
 main(){
  int what = 0;
- int r;
+ int r, n;
+ int i = 1;
+ uint64_t t1, t2; 
  printf("what = %d\n",what);
+ t1 = rdtsc();
 #pragma omp parallel for
- for(r=0 ; r<1200 ; r++) {
- atomic_add(&what,r);
+ for(r=0 ; r<1000000 ; r++) {
+   n=atomic_fetch_and_add(&what,i);
 //  atomic_add(&what,2);
 //  atomic_xor(&what,3);
 //  atomic_xor(&what,3);
@@ -54,6 +84,7 @@ main(){
 //  atomic_add(&what,3);
 // printf("%d %3d what = %3d\n",omp_get_thread_num(),r,what);
  }
- printf("what = %d\n",what);
+ t2 = rdtsc();
+ printf("what = %10d, n= %10d, time = %10ld\n",what,n,t2-t1);
 }
 #endif
