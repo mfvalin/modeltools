1c97d69dfe401a3196afb3f006391d32aa70a357
Michel Valin <mfvalin@gmail.com>
2017-10-26 18:42:02 -0400

    added compilation to demo

--- mgilib_shm/run_demo.sh
+++ mgilib_shm/run_demo.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -x
+mpicc -o server_1 server_1.c
+mpicc -o client_1 client_1.c
 rm -f DemoClientServerPort
-mpirun -n 1 server_1 &
+mpirun -n 1 ./server_1 &
 sleep 2
 mpirun -n 2 ./client_1 "$(cat DemoClientServerPort)"
 wait
+rm -f DemoClientServerPort
