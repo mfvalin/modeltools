a25a12395b49f08ec96d38c97863c9495b69fc84
Michel Valin <mfvalin@gmail.com>
2017-12-03 10:06:21 -0500

    test program refinements

--- RAND/random_test.c
+++ RAND/random_test.c
@@ -1,9 +1,10 @@
 #include <randomgeneric.h>
 #include <mpi.h>
+#include <math.h>
 
 int main(int argc, char **argv){
-  unsigned int lr;
-  int i, j;
+  unsigned int lr, lr2;
+  int i, j, k, l;
   double t0, t1, rval;
   double MPI_Wtime() ;
   unsigned int ranbuf[1200000];
@@ -17,6 +18,7 @@ int main(int argc, char **argv){
   generic_state *shr3;
   unsigned int piSeed = 123456789;
   int irep = 0;
+  int count[32];
 
   MPI_Init(&argc,&argv);
 
@@ -74,18 +76,29 @@ int main(int argc, char **argv){
 //   pos = 0 ; neg = 0 ; for( i=0 ; i < 1000000 ; i++) if((int)ranbuf[i] > 0) pos++ ; else neg++ ;
 //   pos = 0 ; neg = 0 ; for( i=0 ; i < 1000000 ; i++) if(ranbuf[i] & 1) pos++ ; else neg++ ;
   postot = 0 ; negtot = 0;
-  for (j=0 ; j<100 ; j++) {
+  for(i=0 ; i<32 ; i++) count[i] = 0;
+  for (j=0 ; j<1000 ; j++) {
     VecIRan_generic_stream(shr3, ranbuf, 1000000) ;
-    mask = 1 ;
-    while (mask) {
-      pos = 0 ; neg = 0 ; 
-      for( i=0 ; i < 1000000 ; i++) if(ranbuf[i] & mask) pos++ ; else neg++  ; 
-      postot += pos ; negtot += neg ;
-      mask <<= 1 ;//  printf("%5d ",pos-neg) ;
+    for(k=0 ; k<1000000 ; k++) {
+      l = ranbuf[k];
+      for(i=0 ; i<32 ; i++) { count[i] += (l & 1) ; l >>=1 ; }
     }
+//     mask = 1 ;
+//     while (mask) {
+//       pos = 0 ; neg = 0 ; 
+//       for( i=0 ; i < 1000000 ; i++) if(ranbuf[i] & mask) pos++ ; else neg++  ; 
+//       postot += pos ; negtot += neg ;
+//       mask <<= 1 ;//  printf("%5d ",pos-neg) ;
+//     }
   }
 //   printf("%d\n",postot-negtot);
-  printf("time for 1E+3 x 1E+6 random integer values = %6.3f , pos - neg = %d\n",t1-t0,postot-negtot);
+  for(i=0 ; i<32 ; i++) count[i] -= (j*k/2);
+  for(i=0 ; i<32 ; i++) postot += count[i] ; 
+  rval = j*k ; rval = sqrt(rval); lr = rval*sqrt(32.0); lr2 = rval;
+  printf("time for 1E+3 x 1E+6 random integer values = %6.3f \n  ones - zeroes = %d (32 * %d samples) expected = +-%d (+-%d by bit)\n  by bit / 100 : ",
+	 t1-t0,postot-negtot,j*k, lr,lr2);
+//   printf("time for 1E+3 x 1E+6 random integer values = %6.3f \n",t1-t0);
+  for(i=0 ; i<32 ; i++) printf("%d ",count[i]/100); printf("\n");
 
   for( i=0 ; i < 10 ; i++) VecDRan_generic_stream(shr3, ranbuf2, 1000000) ;
   MPI_Barrier(MPI_COMM_WORLD);
