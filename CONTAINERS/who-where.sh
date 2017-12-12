12ec65d42a61f3530ae9a9fb810a33e3df77a61f
Michel Valin <mfvalin@gmail.com>
2017-12-12 18:47:37 -0500

    ssm builder + a few fixes

--- CONTAINERS/who-where.sh
+++ CONTAINERS/who-where.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export MP_CHILD=${MP_CHILD:-${PMI_RANK:-${OMPI_COMM_WORLD_RANK:-${ALPS_APP_PE}}}}
 echo "CHILD = $MP_CHILD, $(taskset -cp $$) on $(hostname)"
-touch $FLAGDIR/FLAG_$MP_CHILD
+[[ -d "$FLAGDIR" ]] && touch $FLAGDIR/FLAG_$MP_CHILD
+true
