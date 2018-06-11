79bca0d844e4770e0592497b5f7a0dcccd54b9fb
M. Valin <mfvalin@gmail.com>
2018-06-11 17:14:39 -0400

    added no fma option

--- x86_utils/fast_sum_dotpr.c
+++ x86_utils/fast_sum_dotpr.c
@@ -65,9 +65,18 @@ void fast_dot_product_f3(const float *fa, const float *fb, const float *fc, doub
     tdb = _mm256_cvtps_pd(tf0);
     tf0 = _mm_loadu_ps((float *) &fc[j + 0]);
     tdc = _mm256_cvtps_pd(tf0);
+#if defined(WITH_FMA)
     da0 = _mm256_fmadd_pd (tda, tda, da0);
-    db0 = _mm256_fmadd_pd (tda, tdb, db0);
     dc0 = _mm256_fmadd_pd (tdb, tdc, dc0);
+    db0 = _mm256_fmadd_pd (tda, tdb, db0);
+#else
+    tdc = _mm256_mul_pd (tdb, tdc);
+    dc0 = _mm256_add_pd (tdc, dc0);
+    tdb = _mm256_mul_pd (tda, tdb);
+    db0 = _mm256_add_pd (tdb, db0);
+    tda = _mm256_mul_pd (tda, tda);
+    da0 = _mm256_add_pd (tda, da0);
+#endif
 
     tf0 = _mm_loadu_ps((float *) &fa[j + 4]);
     tda = _mm256_cvtps_pd(tf0);
@@ -75,9 +84,18 @@ void fast_dot_product_f3(const float *fa, const float *fb, const float *fc, doub
     tdb = _mm256_cvtps_pd(tf0);
     tf0 = _mm_loadu_ps((float *) &fc[j + 4]);
     tdc = _mm256_cvtps_pd(tf0);
+#if defined(WITH_FMA)
     da1 = _mm256_fmadd_pd (tda, tda, da1);
     db1 = _mm256_fmadd_pd (tda, tdb, db1);
     dc1 = _mm256_fmadd_pd (tdb, tdc, dc1);
+#else
+    tdc = _mm256_mul_pd (tdb, tdc);
+    dc1 = _mm256_add_pd (tdc, dc1);
+    tdb = _mm256_mul_pd (tda, tdb);
+    db1 = _mm256_add_pd (tdb, db1);
+    tda = _mm256_mul_pd (tda, tda);
+    da1 = _mm256_add_pd (tda, da1);
+#endif
   }
   da0 =  _mm256_add_pd (da0, da1);   // add subtotal pairs into da0, db0, dc0
   db0 =  _mm256_add_pd (db0, db1);
@@ -136,25 +154,45 @@ double fast_dot_product_f(const float *f1, const float *f2, int n){
     td0 = _mm256_cvtps_pd(tf0);
     tf0 = _mm_loadu_ps((float *) &f2[j + 0]);
     td1 = _mm256_cvtps_pd(tf0);
+#if defined(WITH_FMA)
     d0 = _mm256_fmadd_pd (td0, td1, d0);
+#else
+    td0 = _mm256_mul_pd(td1, td0);
+    d0 = _mm256_add_pd (td0, d0);
+#endif
 
     tf0 = _mm_loadu_ps((float *) &f1[j + 4]);
     td0 = _mm256_cvtps_pd(tf0);
     tf0 = _mm_loadu_ps((float *) &f2[j + 4]);
     td1 = _mm256_cvtps_pd(tf0);
+#if defined(WITH_FMA)
     d1 = _mm256_fmadd_pd (td0, td1, d1);
+#else
+    td0 = _mm256_mul_pd(td1, td0);
+    d1 = _mm256_add_pd (td0, d1);
+#endif
 
     tf0 = _mm_loadu_ps((float *) &f1[j + 8]);
     td0 = _mm256_cvtps_pd(tf0);
     tf0 = _mm_loadu_ps((float *) &f2[j + 8]);
     td1 = _mm256_cvtps_pd(tf0);
+#if defined(WITH_FMA)
     d2 = _mm256_fmadd_pd (td0, td1, d2);
+#else
+    td0 = _mm256_mul_pd(td1, td0);
+    d2 = _mm256_add_pd (td0, d2);
+#endif
 
     tf0 = _mm_loadu_ps((float *) &f1[j +12]);
     td0 = _mm256_cvtps_pd(tf0);
     tf0 = _mm_loadu_ps((float *) &f2[j +12]);
     td1 = _mm256_cvtps_pd(tf0);
+#if defined(WITH_FMA)
     d3 = _mm256_fmadd_pd (td0, td1, d3);
+#else
+    td0 = _mm256_mul_pd(td1, td0);
+    d3 = _mm256_add_pd (td0, d3);
+#endif
   }
   d0 =  _mm256_add_pd (d0, d1);   // add 4 subtotals into d0
   d2 =  _mm256_add_pd (d2, d3);
@@ -197,19 +235,39 @@ double fast_normdot_f(const float *f1, int n){
   for(j=i ; j<n ; j+=16){      // 16 values processed per iteration
     tf0 = _mm_loadu_ps((float *) &f1[j + 0]);
     td0 = _mm256_cvtps_pd(tf0);
+#if defined(WITH_FMA)
     d0 = _mm256_fmadd_pd (td0, td0, d0);
+#else
+    td0 = _mm256_mul_pd(td0, td0);
+    d0 = _mm256_add_pd (td0, d0);
+#endif
 
     tf0 = _mm_loadu_ps((float *) &f1[j + 4]);
     td0 = _mm256_cvtps_pd(tf0);
+#if defined(WITH_FMA)
     d1 = _mm256_fmadd_pd (td0, td0, d1);
+#else
+    td0 = _mm256_mul_pd(td0, td0);
+    d1 = _mm256_add_pd (td0, d1);
+#endif
 
     tf0 = _mm_loadu_ps((float *) &f1[j + 8]);
     td0 = _mm256_cvtps_pd(tf0);
+#if defined(WITH_FMA)
     d2 = _mm256_fmadd_pd (td0, td0, d2);
+#else
+    td0 = _mm256_mul_pd(td0, td0);
+    d2 = _mm256_add_pd (td0, d2);
+#endif
 
     tf0 = _mm_loadu_ps((float *) &f1[j +12]);
     td0 = _mm256_cvtps_pd(tf0);
+#if defined(WITH_FMA)
     d3 = _mm256_fmadd_pd (td0, td0, d3);
+#else
+    td0 = _mm256_mul_pd(td0, td0);
+    d3 = _mm256_add_pd (td0, d3);
+#endif
   }
   d0 =  _mm256_add_pd (d0, d1);   // add 4 subtotals into d0
   d2 =  _mm256_add_pd (d2, d3);
