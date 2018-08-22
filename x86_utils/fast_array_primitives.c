a2a555a99ba5ac92122f3b2c9b8a67061e26d7b2
Michel Valin <mfvalin@gmail.com>
2018-08-22 12:29:15 -0400

    start of SIMD implementation of endian swap

--- x86_utils/fast_array_primitives.c
+++ x86_utils/fast_array_primitives.c
@@ -26,6 +26,85 @@
 #define ESWAP8x2_128(xmm)                    // 8 in 16
 #define ESWAP8x2_256(ymm)
 
+// endian swap of bytes in 32 bit tokens (s -> d)
+static uint8_t swapindex_8_32[] = {  3,  2,  1,  0,  7,  6,  5,  4, 11, 10,  9,  8, 15, 14, 13, 12};
+void swap_8_in_32(void *s, void *d, int n){
+#if defined(__AVX2__)
+  __m256i ix, vs0, vs1;
+#endif
+  uint32_t i, n2;
+  uint32_t t;
+  uint32_t *s0, *s1;
+  uint32_t *d0, *d1;
+
+  n2 = (n >> 4);        // number of 16 token chunks
+  s0 = (uint32_t *) s;
+  s1 = s0;
+  d0 = (uint32_t *) d;
+  d1 = d0;
+  i = 0;
+#if defined(__AVX2__)
+  s1 = s0 + (n2 << 3);  // "halfway" point
+  d1 = d0 + (n2 << 3);  // "halfway" point
+  ix = _mm256_loadu_si256((__m256i const *) swapindex_8_32);
+  for( ; i < n - 15 ; i += 16){                        // endian swap of 16 32 bit tokens
+    vs0 = _mm256_loadu_si256((__m256i const *) s0);    // load 8 32 bit tokens
+    vs1 = _mm256_loadu_si256((__m256i const *) s1);    // load 8 32 bit tokens
+    vs0 = _mm256_shuffle_epi8 (vs0, ix);               // shuffle (vector bswap equivalent)
+    vs1 = _mm256_shuffle_epi8 (vs1, ix);               // shuffle (vector bswap equivalent)
+    _mm256_storeu_si256((__m256i *) d0, vs0);          // store 8 32 bit tokens
+    _mm256_storeu_si256((__m256i *) d1, vs1);          // store 8 32 bit tokens
+    s0 += 8;
+    s1 += 8;
+    d0 += 8;
+    d1 += 8;
+  }
+#endif
+  for( ; i < n ; i++){    // loop over remainder
+    t = *s1++;
+    t = (t >> 24) | (t << 24) | ((t >> 8) & 0xFF00) | ((t & 0xFF00) << 8);    // bswap
+    *d1++ = t;
+  }
+}
+
+// endian swap of 32 bit tokens in 64 bit tokens (s -> d)
+void swap_32_in_64(void *s, void *d, int n){
+#if defined(__AVX2__)
+  __m256i vs0, vs1;
+#endif
+  uint32_t i, n2;
+  uint64_t t;
+  uint32_t *s0, *s1;
+  uint32_t *d0, *d1;
+
+  n2 = (n >> 3);        // number of 8 token chunks
+  s0 = (uint32_t *) s;
+  s1 = s0;
+  d0 = (uint32_t *) d;
+  d1 = d0;
+  i = 0;
+#if defined(__AVX2__)
+  s1 = s0 + (n2 << 2);  // "halfway" point
+  d1 = d0 + (n2 << 2);  // "halfway" point
+  for( ; i < n - 7 ; i +=8){                           // endian swap of 8 64 bit tokens
+    vs0 = _mm256_loadu_si256((__m256i const *) s0);    // load 4 64 bit tokens
+    vs1 = _mm256_loadu_si256((__m256i const *) s1);    // load 4 64 bit tokens
+    vs0 = _mm256_shuffle_epi32(vs0 , 0x1B);
+    vs1 = _mm256_shuffle_epi32(vs1 , 0x1B);
+    _mm256_storeu_si256((__m256i *) d0, vs0);          // store 4 64 bit tokens
+    _mm256_storeu_si256((__m256i *) d1, vs1);          // store 4 64 bit tokens
+    s0 += 4;
+    s1 += 4;
+    d0 += 4;
+    d1 += 4;
+  }
+#endif
+  for( ; i < n ; i++){    // loop over remainder
+    t = *s1++;
+    t = (t >> 32) | (t << 32);
+    *d1++ = t;
+  }
+}
 
 // get min value, max value, very rough average of 32 bit float array
 // the average is only ~OK if n is a multiple of 2 * VLEN
