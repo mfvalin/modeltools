e81fe9e879fff73b350c6010165b20b9a1e00388
M. Valin <mfvalin@gmail.com>
2018-10-05 13:09:00 -0400

    added single field version

--- x86_utils/tricublin_3f_d.c
+++ x86_utils/tricublin_3f_d.c
@@ -20,8 +20,8 @@
 #include <immintrin.h>
 #include <stdint.h>
 
-static double cp167 =  0.1666666667;
-static double cm167 = -0.1666666667;
+static double cp167 =  1.0/6.0;
+static double cm167 = -1.0/6.0;
 static double cp5 = 0.5;
 static double cm5 = -0.5;
 static double one = 1.0;
@@ -36,6 +36,36 @@ void Bicubic_coeffs_d(double *px, double *py, double x, double y){
   py[1] = cp5*(y+one)*(y-one)*(y-two);
   py[2] = cm5*y*(y+one)*(y-two);
   py[3] = cp167*y*(y+one)*(y-one);
+
+  px[0] = cm167*x*(x-one)*(x-two);        // coefficients for interpolation along x
+  px[1] = cp5*(x+one)*(x-one)*(x-two);
+  px[2] = cm5*x*(x+one)*(x-two);
+  px[3] = cp167*x*(x+one)*(x-one);
+}
+
+// same as above, but three dimensions
+// constant interval between points is assumed
+void Tricubic_coeffs_d(double *px, double *py, double *pz, double x, double y, double z){
+
+  pz[0] = cm167*z*(z-one)*(z-two);        // coefficients for interpolation along z
+  pz[1] = cp5*(z+one)*(z-one)*(z-two);
+  pz[2] = cm5*z*(z+one)*(z-two);
+  pz[3] = cp167*z*(z+one)*(z-one);
+
+  py[0] = cm167*y*(y-one)*(y-two);        // coefficients for interpolation along y
+  py[1] = cp5*(y+one)*(y-one)*(y-two);
+  py[2] = cm5*y*(y+one)*(y-two);
+  py[3] = cp167*y*(y+one)*(y-one);
+
+  px[0] = cm167*x*(x-one)*(x-two);        // coefficients for interpolation along x
+  px[1] = cp5*(x+one)*(x-one)*(x-two);
+  px[2] = cm5*x*(x+one)*(x-two);
+  px[3] = cp167*x*(x+one)*(x-one);
+}
+
+// same as above, but only one dimension
+// constant interval between points is assumed
+void Cubic_coeffs_d(double *px, double x){
 
   px[0] = cm167*x*(x-one)*(x-two);        // coefficients for interpolation along x
   px[1] = cp5*(x+one)*(x-one)*(x-two);
@@ -219,9 +249,9 @@ void Tricublin_zyxf3_d(float *d, float *f1, float *f2, float *f3, double *px, do
   int ninj = NINJ;
   int ninjl;    // ninj (cubic along z) or 0 (linear along z)
   int ni2, ni3;
+#if defined(__AVX2__) && defined(__x86_64__)
   float *s1, *s2, *s3;
   int32_t *l = (int32_t *)d;
-#if defined(__AVX2__) && defined(__x86_64__)
   __m256d cz0, cz1, cz2, cz3, cy, cx;
   __m256d x0;
   __m256d za, zb, zc;
@@ -229,8 +259,8 @@ void Tricublin_zyxf3_d(float *d, float *f1, float *f2, float *f3, double *px, do
   __m128d va, vb, vc;
   __m128  ta, tb, tc;
 #else
-  double va4[12], vb4[12], vc4[12], vd4[12];
-  int ninj2, ninj3;
+  double va4[4], vb4[4], vc4[4], vd4[4], dst[4];
+  int i, ninj2, ninj3;
 #endif
   
   ni2 = ni + ni;
@@ -350,8 +380,139 @@ void Tricublin_zyxf3_d(float *d, float *f1, float *f2, float *f3, double *px, do
 #else
   ninj2 = ninj + ninjl;
   ninj3 = ninj2 + ninjl;
+  // field 1
   for (i=0 ; i<4 ; i++){
+    va4[i] = f1[i    ]*pz[0] + f1[i    +ninj]*pz[1] +  f1[i    +ninj2]*pz[2] + f1[i    +ninj3]*pz[3];
+    vb4[i] = f1[i+ni ]*pz[0] + f1[i+ni +ninj]*pz[1] +  f1[i+ni +ninj2]*pz[2] + f1[i+ni +ninj3]*pz[3];
+    vc4[i] = f1[i+ni2]*pz[0] + f1[i+ni2+ninj]*pz[1] +  f1[i+ni2+ninj2]*pz[2] + f1[i+ni2+ninj3]*pz[3];
+    vd4[i] = f1[i+ni3]*pz[0] + f1[i+ni3+ninj]*pz[1] +  f1[i+ni3+ninj2]*pz[2] + f1[i+ni3+ninj3]*pz[3];
+    dst[i] = va4[i]*py[0] + vb4[i]*py[1] + vc4[i]*py[2] + vd4[i]*py[3];
   }
+  d[0] = dst[0]*px[0] + dst[1]*px[1] + dst[2]*px[2] + dst[3]*px[3];
+  // field 2
+  for (i=0 ; i<4 ; i++){
+    va4[i] = f2[i    ]*pz[0] + f2[i    +ninj]*pz[1] +  f2[i    +ninj2]*pz[2] + f2[i    +ninj3]*pz[3];
+    vb4[i] = f2[i+ni ]*pz[0] + f2[i+ni +ninj]*pz[1] +  f2[i+ni +ninj2]*pz[2] + f2[i+ni +ninj3]*pz[3];
+    vc4[i] = f2[i+ni2]*pz[0] + f2[i+ni2+ninj]*pz[1] +  f2[i+ni2+ninj2]*pz[2] + f2[i+ni2+ninj3]*pz[3];
+    vd4[i] = f2[i+ni3]*pz[0] + f2[i+ni3+ninj]*pz[1] +  f2[i+ni3+ninj2]*pz[2] + f2[i+ni3+ninj3]*pz[3];
+    dst[i] = va4[i]*py[0] + vb4[i]*py[1] + vc4[i]*py[2] + vd4[i]*py[3];
+  }
+  d[1] = dst[0]*px[0] + dst[1]*px[1] + dst[2]*px[2] + dst[3]*px[3];
+  // field 3
+  for (i=0 ; i<4 ; i++){
+    va4[i] = f3[i    ]*pz[0] + f3[i    +ninj]*pz[1] +  f3[i    +ninj2]*pz[2] + f3[i    +ninj3]*pz[3];
+    vb4[i] = f3[i+ni ]*pz[0] + f3[i+ni +ninj]*pz[1] +  f3[i+ni +ninj2]*pz[2] + f3[i+ni +ninj3]*pz[3];
+    vc4[i] = f3[i+ni2]*pz[0] + f3[i+ni2+ninj]*pz[1] +  f3[i+ni2+ninj2]*pz[2] + f3[i+ni2+ninj3]*pz[3];
+    vd4[i] = f3[i+ni3]*pz[0] + f3[i+ni3+ninj]*pz[1] +  f3[i+ni3+ninj2]*pz[2] + f3[i+ni3+ninj3]*pz[3];
+    dst[i] = va4[i]*py[0] + vb4[i]*py[1] + vc4[i]*py[2] + vd4[i]*py[3];
+  }
+  d[2] = dst[0]*px[0] + dst[1]*px[1] + dst[2]*px[2] + dst[3]*px[3];
+#endif
+}
+
+
+// same as above but for one array f1(ni,nj,nk)
+void Tricublin_zyxf_d(float *d, float *f1, double *px, double *py, double *pz, int NI, int NINJ){
+  int ni = NI;
+  int ninj = NINJ;
+  int ninjl;    // ninj (cubic along z) or 0 (linear along z)
+  int ni2, ni3;
+#if defined(__AVX2__) && defined(__x86_64__)
+  float *s1;
+  int32_t *l = (int32_t *)d;
+  __m256d cz0, cz1, cz2, cz3, cy, cx;
+  __m256d x0;
+  __m256d za;
+  __m256d ya;
+  __m128d va;
+  __m128  ta;
+#else
+  double va4[4], vb4[4], vc4[4], vd4[4], dst[4];
+  int i, ninj2, ninj3;
+#endif
+  
+  ni2 = ni + ni;
+  ni3 = ni2 + ni;
+  ninjl = ninj ; // assuming cubic case. in the linear case, ninjl will be set to 0
+  if(pz[2] == 0.0 && pz[3] == 0.0) ninjl = 0;
+
+#if defined(__AVX2__) && defined(__x86_64__)
+  // ==== interpolation along Z, vector length is 12 (3 vectors of length 4 per plane) ====
+  cz0 = _mm256_broadcast_sd(pz  );   // coefficients for interpolation along z, promote to vectors
+  cz1 = _mm256_broadcast_sd(pz+1);
+  cz2 = _mm256_broadcast_sd(pz+2);
+  cz3 = _mm256_broadcast_sd(pz+3);
+
+  s1 = f1;      // row 0, 4 planes (Z0, Z1, Z2, Z3)
+  cy  = _mm256_broadcast_sd(py);
+
+  za  = _mm256_mul_pd( _mm256_cvtps_pd(_mm_load_ps(s1)), cz0);           // plane 0
+  s1 += ninj;
+  za  = _mm256_fmadd_pd( _mm256_cvtps_pd(_mm_load_ps(s1)),cz1,za);       // plane 1
+  s1 += ninjl;
+  za  = _mm256_fmadd_pd( _mm256_cvtps_pd(_mm_load_ps(s1)),cz2,za);       // plane 2
+  s1 += ninjl;
+  za  = _mm256_fmadd_pd( _mm256_cvtps_pd(_mm_load_ps(s1)),cz3,za);       // plane 3
+
+  ya  = _mm256_mul_pd(za,cy);
+
+  s1 = f1 + ni;        // row 1, 4 planes (Z0, Z1, Z2, Z3)
+  cy  = _mm256_broadcast_sd(py+1);
+
+  za  = _mm256_mul_pd( _mm256_cvtps_pd(_mm_load_ps(s1)), cz0);           // plane 0
+  s1 += ninj;
+  za  = _mm256_fmadd_pd( _mm256_cvtps_pd(_mm_load_ps(s1)),cz1,za);       // plane 1
+  s1 += ninjl;
+  za  = _mm256_fmadd_pd( _mm256_cvtps_pd(_mm_load_ps(s1)),cz2,za);       // plane 2
+  s1 += ninjl;
+  za  = _mm256_fmadd_pd( _mm256_cvtps_pd(_mm_load_ps(s1)),cz3,za);       // plane 3
+
+  ya  = _mm256_fmadd_pd(za,cy,ya);
+
+  s1 = f1 + ni2;     // row 2, 4 planes (Z0, Z1, Z2, Z3)
+  cy  = _mm256_broadcast_sd(py+2);
+
+  za  = _mm256_mul_pd( _mm256_cvtps_pd(_mm_load_ps(s1)), cz0);           // plane 0
+  s1 += ninj;
+  za  = _mm256_fmadd_pd( _mm256_cvtps_pd(_mm_load_ps(s1)),cz1,za);       // plane 1
+  s1 += ninjl;
+  za  = _mm256_fmadd_pd( _mm256_cvtps_pd(_mm_load_ps(s1)),cz2,za);       // plane 2
+  s1 += ninjl;
+  za  = _mm256_fmadd_pd( _mm256_cvtps_pd(_mm_load_ps(s1)),cz3,za);       // plane 3
+
+  ya  = _mm256_fmadd_pd(za,cy,ya);
+
+  s1 = f1 + ni3;     // row 3, 4 planes (Z0, Z1, Z2, Z3)
+  cy  = _mm256_broadcast_sd(py+3);
+
+  za  = _mm256_mul_pd( _mm256_cvtps_pd(_mm_load_ps(s1)), cz0);           // plane 0
+  s1 += ninj;
+  za  = _mm256_fmadd_pd( _mm256_cvtps_pd(_mm_load_ps(s1)),cz1,za);       // plane 1
+  s1 += ninjl;
+  za  = _mm256_fmadd_pd( _mm256_cvtps_pd(_mm_load_ps(s1)),cz2,za);       // plane 2
+  s1 += ninjl;
+  za  = _mm256_fmadd_pd( _mm256_cvtps_pd(_mm_load_ps(s1)),cz3,za);       // plane 3
+
+  ya  = _mm256_fmadd_pd(za,cy,ya);
+
+  // interpolation along x
+  cx = _mm256_load_pd(px) ;
+  ya = _mm256_mul_pd(ya, cx );   // multiply by px
+  // use folding to sum 4 terms
+  va = _mm_add_pd( _mm256_extractf128_pd(ya,0) , _mm256_extractf128_pd(ya,1) ); va = _mm_hadd_pd(va,va);
+  ta = _mm_cvtpd_ps(va); l[0] = _mm_extract_epi32((__m128i) ta, 0);  // convert results to float and store
+#else
+  ninj2 = ninj + ninjl;
+  ninj3 = ninj2 + ninjl;
+  // field 1
+  for (i=0 ; i<4 ; i++){
+    va4[i] = f1[i    ]*pz[0] + f1[i    +ninj]*pz[1] +  f1[i    +ninj2]*pz[2] + f1[i    +ninj3]*pz[3];
+    vb4[i] = f1[i+ni ]*pz[0] + f1[i+ni +ninj]*pz[1] +  f1[i+ni +ninj2]*pz[2] + f1[i+ni +ninj3]*pz[3];
+    vc4[i] = f1[i+ni2]*pz[0] + f1[i+ni2+ninj]*pz[1] +  f1[i+ni2+ninj2]*pz[2] + f1[i+ni2+ninj3]*pz[3];
+    vd4[i] = f1[i+ni3]*pz[0] + f1[i+ni3+ninj]*pz[1] +  f1[i+ni3+ninj2]*pz[2] + f1[i+ni3+ninj3]*pz[3];
+    dst[i] = va4[i]*py[0] + vb4[i]*py[1] + vc4[i]*py[2] + vd4[i]*py[3];
+  }
+  d[0] = dst[0]*px[0] + dst[1]*px[1] + dst[2]*px[2] + dst[3]*px[3];
 #endif
 }
 
@@ -364,6 +525,9 @@ void Tricublin_zyxf3_d(float *d, float *f1, float *f2, float *f3, double *px, do
 
 int main(int argc, char **argv){
   float array[3*NI*NJ*NK];
+  float a1[NI*NJ*NK];
+  float a2[NI*NJ*NK];
+  float a3[NI*NJ*NK];
   float dest[3*NI*NJ*NK];
   double pz[4], px[4], py[4];
   float avg=0.0;
@@ -373,16 +537,21 @@ int main(int argc, char **argv){
   long long tm1, tm2;
   int i,j,k,ijk;
   float *p = array;
+  float *p1 = a1;
+  float *p2 = a2;
+  float *p3 = a3;
   int nijk = NI*NJ*NK - 5*NI*NJ;
   int II, JJ, KK;
 
-  printf("beep \n");
   for (k=0 ; k<NK ; k++ ){
     for (j=0 ; j<NJ ; j++ ){
       for (i=0 ; i<NI ; i++ ){
         p[0] = i + j + k;
         p[1] = p[0] + 10;
         p[2] = p[0] + 100;
+        *p1++ = p[0];
+        *p2++ = p[1];
+        *p3++ = p[2];
         p += 3;
       }
     }
@@ -416,7 +585,18 @@ int main(int argc, char **argv){
     expected2 = II + 1 + dx + JJ + 1 + dy + KK + 1 + dz ;
   }
   printf("pz  =  %10.6f %10.6f %10.6f %10.6f\n",pz[0],pz[1],pz[2],pz[3]);
+  dest[0] = -1.0 ; dest[1] = -1.0 ; dest[2] = -1.0 ;
   Tricublin_zyx3f_d(&dest[0], &array[II*3 + JJ*NI*3 + KK*NI*NJ*3], px, py, pz, NI, NI*NJ);
+  printf("got %10.6f, %10.6f, %10.6f, expected %10.6f %10.6f %10.6f\n",dest[0],dest[1],dest[2],expected2,expected2+10,expected2+100);
+
+  dest[0] = -1.0 ; dest[1] = -1.0 ; dest[2] = -1.0 ;
+  Tricublin_zyxf3_d(&dest[0], &a1[II + JJ*NI + KK*NI*NJ], &a2[II + JJ*NI + KK*NI*NJ], &a3[II + JJ*NI + KK*NI*NJ], px, py, pz, NI, NI*NJ);
+  printf("got %10.6f, %10.6f, %10.6f, expected %10.6f %10.6f %10.6f\n",dest[0],dest[1],dest[2],expected2,expected2+10,expected2+100);
+
+  dest[0] = -1.0 ; dest[1] = -1.0 ; dest[2] = -1.0 ;
+  Tricublin_zyxf_d(&dest[0], &a1[II + JJ*NI + KK*NI*NJ], px, py, pz, NI, NI*NJ);
+  Tricublin_zyxf_d(&dest[1], &a2[II + JJ*NI + KK*NI*NJ], px, py, pz, NI, NI*NJ);
+  Tricublin_zyxf_d(&dest[2], &a3[II + JJ*NI + KK*NI*NJ], px, py, pz, NI, NI*NJ);
   printf("got %10.6f, %10.6f, %10.6f, expected %10.6f %10.6f %10.6f\n",dest[0],dest[1],dest[2],expected2,expected2+10,expected2+100);
 }
 #endif
