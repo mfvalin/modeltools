b9757079b8acf1efdaca7206004f6a638c28cdbc
M. Valin <mfvalin@gmail.com>
2019-02-25 19:49:03 -0500

    added gnu atomic fetch and add for intel icc

--- x86_utils/barriers_and_locks.c
+++ x86_utils/barriers_and_locks.c
@@ -1,18 +1,18 @@
-/* RMNLIB - Library of useful routines for C and FORTRAN programming
+/* 
  * Copyright (C) 2018  Environnement Canada
  *
- * This library is free software; you can redistribute it and/or
+ * This is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation,
  * version 2.1 of the License.
  *
- * This library is distributed in the hope that it will be useful,
+ * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
+ * License along with this software; if not, write to the
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
@@ -27,6 +27,18 @@
 #if defined(SELF_TEST)
 #include <stdio.h>
 #include <mpi.h>
+#endif
+
+#if defined __INTEL_COMPILER
+static inline int32_t __sync_fetch_and_add(int32_t* ptr, int32_t value) {
+        int32_t previous;
+        asm volatile ("lock;\n"
+                "\txaddl %1, %2;"
+                : "=r"(previous)
+                : "0"(value), "m"(*ptr)
+                : "memory");
+        return previous;
+}
 #endif
 
 // static int volatile __attribute__ ((aligned (64))) spins[64];
