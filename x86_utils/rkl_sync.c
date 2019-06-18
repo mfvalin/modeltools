99243f02cbd5c06974cb529aafad657ae2d87f62
M. Valin <mfvalin@gmail.com>
2019-06-18 17:41:40 -0400

    starting fortran nterfaces and documentation for auto extraction

--- x86_utils/rkl_sync.c
+++ x86_utils/rkl_sync.c
@@ -11,6 +11,58 @@
 //     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 //     Lesser General Public License for more details.
 
+//****P* librkl/RPN kernel library locks and barriers
+// DESCRIPTION
+// set of routines to implement locks and barriers that can be used between threads belonging
+// to the same process or between processes running on the same shared memory node (MPI, etc...)
+//
+#if defined(NEVER_EVER_TRUE)
+// EXAMPLES
+  =============  increment under lock =============
+
+  static volatile int my_lock = 0;
+  int kount;
+  ......
+  AcquireLock(&my_lock);  // acquire lock
+  kount++;                // increment variable under lock
+  ReleaseLock(&my_lock);  // release lock
+
+  ......
+  AcquireIdLock(&my_lock, id);  // acquire lock number id (0 -> maxitems-1 , see setup_locks_and_barriers)
+  kount++;                // increment variable under lock
+  ReleaseIdLock(&my_lock, id);  // release lock number id
+
+  =============  get a block of shared memory =============
+
+  void *ptr
+  int sid, localrank;
+  int size = ....;
+    .....
+    
+  ierr = MPI_Comm_rank(MY_World,&localrank);         // rank of this PE on this SMP node
+  if(localrank == 0){
+    ptr = allocate_safe_shared_memory(&sid, size);   // create a shared memory segment and get it's address
+    ierr = MPI_Bcast(&sid,1,MPI_INTEGER,0,MY_World); // broadcast the segment's id
+  }else{                                             // other PEs
+    ierr = MPI_Bcast(&sid,1,MPI_INTEGER,0,MY_World); // get the segment's id
+    ptr = shmat(sid,NULL,0);                         // get the shared memory segment's address
+  }
+  ierr = MPI_Barrier(MY_World);                      // wait for all PEs to have attached the segment
+
+  =============  setup of a block of shared memory for barriers and locks =============
+
+  int *next;
+  next = (int *) setup_locks_and_barriers(ptr, size, 16);  // 16 locks, 16 barriers, point next to area that follows
+  // only on rank 0 normally
+
+  =============  an intra-node barrier =============
+
+  ierr = MPI_Comm_rank(MY_World,&localsize);         // how many PEs in MY_World on this SMP node
+    .....
+  simple_node_barrier(0, localsize);                 // use barrier 0 (0 -> maxitems-1 , see setup_locks_and_barriers)
+//****
+#endif
+
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/ipc.h>
@@ -18,6 +70,8 @@
 #include <sys/stat.h>
 
 #include <rkl_sync.h>
+
+// interface   !InTf!
 
 // default number of locks/barriers = 16
 #define MAXLOCKID 15
@@ -35,11 +89,16 @@ static uint32_t volatile *barrs;       // in case we want to allocate to another
 static uint32_t volatile count0[MAXLOCKID+1] __attribute__ ((aligned (64)));
 static uint32_t volatile *count;       // in case we want to allocate to another address
 
+//****f* librkl/allocate_safe_shared_memory
+// Synopsis
 // allocate a  shared memory segment of size Bytes 
 // segment will be preventively marked for deletion (only works on linux)
 // return address and id of shared memory segment (in sid)
 // in case of error id is <0 and the returned address is NULL
-void *allocate_safe_shared_memory(int32_t *sid, uint32_t size)   // InTc
+//
+// ARGUMENTS
+void *allocate_safe_shared_memory(int32_t *sid, uint32_t size)   // !InTc!
+//****
 {
   size_t siz = size;
   int32_t id;
@@ -55,12 +114,19 @@ void *allocate_safe_shared_memory(int32_t *sid, uint32_t size)   // InTc
   return ptr;                     // return address of shared memory segment
 }
 
-// switch to tables of a different dimension at another address
-// p        : base 
-// size     : in bytes
+//****f* librkl/setup_locks_and_barriers
+// Synopsis
+// use memory pointed to by p to set up control tables for maxitems locks and barriers
+// if there is not enough memory, return NULL
+//
+// p        : base address
+// size     : amount of available memory in bytes
 // maxitems : number of locks/barriers to allocate (will be rounded down to a multiple of 16)
-// return the next available address
-void *setup_locks_and_barriers(void *p, uint32_t size, uint32_t maxitems)   // InTc
+// return the next available address (above tables)
+//
+// ARGUMENTS
+void *setup_locks_and_barriers(void *p, uint32_t size, uint32_t maxitems)   // !InTc!
+//****
 {
   maxitems = (maxitems >> 4) << 4;  // multiple of 16 x 4 byte items
   if(size < (maxitems * 3 * sizeof(uint32_t)) ) return NULL;   // not enough memory for tables, OOPS
@@ -68,23 +134,36 @@ void *setup_locks_and_barriers(void *p, uint32_t size, uint32_t maxitems)   // I
   locks = (uint32_t *) p;
   barrs = locks + maxitems;
   count = barrs + maxitems;
-  return (void *)(count + maxitems);     // next free address
+  return (void *)(count + maxitems);     // next free address (after lock/barrier tables)
 }
 
-// unsophisticated version, probably fastest up to 8 participants
-// id = which barrier is to be used
-// version with "flag flip"
-uint32_t simple_node_barrier(int32_t id, int32_t maxcount)   // InTc
+//****f* librkl/simple_node_barrier
+// Synopsis
+// implement a barrier between threads or processes (on the same SMP node)
+// unsophisticated version (uses "flag flip")
+// id       : barrier (0 -> maxitems-1) is to be used (see setup_locks_and_barriers)
+// maxcount : number of threads/processes for this barrier
+//
+// the function will return 0 upon success, 1 in case of error (invalid id)
+//
+// Fortran interface
+//   function simple_node_barrier(id, maxcount) result(status)   !InTf!
+//     import :: C_INT                                           !InTf!
+//     integer(C_INT), intent(IN), value :: id, maxcount         !InTf!
+//     integer(C_INT) :: status                                  !InTf!
+//   end function simple_node_barrier                            !InTf!
+// ARGUMENTS
+uint32_t simple_node_barrier(int32_t id, int32_t maxcount)   // !InTc!
+//****
 {
   int lgo, cnt;
 
-  if(id < 0 || id >  maxlockid) return 1;  // bad id
+  if(id < 0 || id >  maxlockid) return 1;  // invalid id
   if(maxcount == 1) return 0;              // trivial case, no need for fancy footwork
 
   lgo = barrs[id];                         // save current flag value
   cnt = __sync_fetch_and_add (count+id, 1);   // increment barrier count
-  if(cnt == maxcount - 1){                 // last participant
-//     count[id] = 0;                         // reset count
+  if(cnt == maxcount - 1){                 // last participant arriving at barrier
     cnt = __sync_fetch_and_add (count+id, -maxcount);  // reset count to zero
     barrs[id] = 1 - barrs[id];             // invert flag
   }else{                                   // all other participants
@@ -96,7 +175,7 @@ uint32_t simple_node_barrier(int32_t id, int32_t maxcount)   // InTc
 #if defined(TEST_MULTI)
 // more sophisticated version with 2 level "flag flip" (may be faster for large counts)
 // id is this PEs identifier (0 < maxcount)
-uint32_t node_barrier_multi(int32_t id, int32_t maxcount)   // InTc
+uint32_t node_barrier_multi(int32_t id, int32_t maxcount)   // !InTc!
 {  
   int lgo, lgo2, cnt;
   int group, base, allgrps, grpcnt, maxid;
@@ -132,15 +211,61 @@ uint32_t node_barrier_multi(int32_t id, int32_t maxcount)   // InTc
 }
 #endif
 
-void AcquireLock(volatile int32_t *lock)   // InTc
+//****f* librkl/AcquireLock
+// Synopsis
+// acquire a lock, using 4 byte variable at address lock
+// the variable pointed to by lock must have been initialized to zero
+// lock   : address of lock variable
+//
+// ARGUMENTS
+void AcquireLock(volatile int32_t *lock)   // !InTc!
+//****
 {
   acquire_lock(lock) ;
 }
 
-void ReleaseLock(volatile int32_t *lock)   // InTc
+//****f* librkl/AcquireIdLock
+// Synopsis
+// acquire a lock, using 4 byte variable at address lock
+// the variable pointed to by lock must have been initialized to zero
+// and will be set to id when lock is acquired
+// lock   : address of lock variable
+// id     : identifier for this thread/process
+//
+// ARGUMENTS
+void AcquireIdLock(volatile int32_t *lock, int32_t id)   // !InTc!
+//****
+{
+  acquire_idlock(lock, id) ;
+}
+
+//****f* librkl/ReleaseLock
+// Synopsis
+// release a lock acquired via AcquireLock using 4 byte variable at address lock
+// attempting to release a lock that is not acquired will result in a deadlock
+//
+// ARGUMENTS
+void ReleaseLock(volatile int32_t *lock)   // !InTc!
+//****
 {
   release_lock(lock) ;
 }
+
+//****f* librkl/ReleaseIdLock
+// Synopsis
+// release a lock acquired via AcquireIdLock using 4 byte variable at address lock
+// attempting to release a lock that was not acquired with this id will result in a deadlock
+// lock   : address of lock variable
+// id     : identifier for this thread/process
+//
+// ARGUMENTS
+void ReleaseIdLock(volatile int32_t *lock, int32_t id)   // !InTc!
+//****
+{
+  release_idlock(lock, id) ;
+}
+
+// end interface   !InTf!
 
 #if defined(SELF_TEST)
 
@@ -171,7 +296,6 @@ int main(int argc, char **argv){
   double tmin, tmax, tavg, tmp;
   volatile int32_t *kount;
   double tall[2048];
-  int it;
 
   ierr = MPI_Init( &argc, &argv );
   ierr = MPI_Comm_rank(MPI_COMM_WORLD,&globalrank);
--- x86_utils/rkl_sync.h
+++ x86_utils/rkl_sync.h
@@ -11,11 +11,13 @@
 //     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 //     Lesser General Public License for more details.
 
+// set of inline functions used to implement locks
+
 static inline void acquire_idlock(volatile void *lock, int32_t id){
   while(__sync_val_compare_and_swap((volatile uint32_t *)lock, 0, id) != 0) ;
 }
 
-static inline void acquire_lock(volatile void *lock){
+static inline void acquire_lock(volatile void *lock){   // no id, use 1
   acquire_idlock(lock, 1) ;
 }
 
@@ -25,19 +27,19 @@ static inline void release_idlock(volatile void *lock, int32_t id){
 }
 
 // this will deadlock if attempt is made to release a lock with id other than 1
-static inline void release_lock(volatile void *lock){
+static inline void release_lock(volatile void *lock){   // no id, use 1
   release_idlock(lock, 1) ;
 }
 
 static inline int32_t test_idlock(volatile void *lock, int32_t id){
-  return (*(volatile uint32_t *)lock != id );
+  return (*(volatile uint32_t *)lock != id );   // true if locked with id
 }
 
-static inline int32_t test_lock0(volatile void *lock){
-  return (*(volatile uint32_t *)lock != 0 );
+static inline int32_t test_lock(volatile void *lock){
+  return (*(volatile uint32_t *)lock != 0 );   // true if locked with any id
 }
 
-// forcefully reset lock
-static inline void force_reset_lock(volatile void *lock){
+
+static inline void force_reset_lock(volatile void *lock){   // forcefully reset lock
   *(volatile uint32_t *)lock = 0 ;
 }
